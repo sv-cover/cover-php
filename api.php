b5236e4adce7aa0de2b8d658261d82a1448b01ef
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2013-11-29 13:45:02 +0100

    Add user login to api for remote login

--- api.php
+++ api.php
@@ -26,6 +26,79 @@ class ControllerApi extends Controller
 		return $activities;
 	}
 
+	public function api_session_create($email, $password)
+	{
+		$user_model = get_model('DataModelMember');
+
+		$member = $user_model->login($email, md5($password));
+
+		if (!$member)
+			return array('result' => false, 'error' => 'User not found');
+
+		$session_model = get_model('DataModelSession');
+
+		$session = $session_model->create($member->get('id'));
+
+		return array('result' => $session->get('id'), 'details' => $member->data);
+	}
+
+	public function api_session_destroy($session_id)
+	{
+		return $session_model->destroy($session_id);
+	}
+
+	public function session_get_member($session_id)
+	{
+		// Get the session
+		$session_model = get_model('DataModelSession');
+
+		$session = $session_model->get($session_id);
+
+		if (!$session)
+			return array('result' => false, 'error' => 'Session not found');
+
+		$user_model = get_model('DataModelMember');
+
+		$member = $user_model->get($session->get('member_id'));
+
+		if (!$member)
+			return array('result' => false, 'error' => 'Member not found');
+
+		return array('result' => $member->data);
+	}
+
+	public function api_session_test_committee($session_id, $committee)
+	{
+		// Get the session
+		$session_model = get_model('DataModelSession');
+
+		$session = $session_model->get($session_id);
+
+		if (!$session)
+			return array('result' => false, 'error' => 'Session not found');
+
+		// Find the committee id
+		$committee_model = get_model('DataModelCommissie');
+
+		$committee = $committee_model->get_from_name($committee);
+
+		if (!$committee)
+			return array('result' => false, 'error' => 'Committee not found');
+
+		// Find in which committees the member is active
+		$member_model = get_model('DataModelMember');
+
+		$member_committees = $member_model->get_commissies($session->get('member_id'));
+
+		if (empty($member_committees))
+			return array('result' => false, 'error' => 'No committees found for this member');
+
+		// And finally, test whether the searched for committee and the member is committees intersect
+		$member_in_committee = in_array($committee->get('id'), $member_committees);
+
+		return array('result' => $member_in_committee);
+	}
+
 	public function run_impl()
 	{
 		$method = isset($_GET['method'])
@@ -36,6 +109,22 @@ class ControllerApi extends Controller
 		{
 			case 'agenda':
 				$response = $this->api_agenda();
+				break;
+
+			case 'session_create':
+				$response = $this->api_session_create($_POST['email'], $_POST['password']);
+				break;
+
+			case 'session_destroy':
+				$response = $this->api_session_destroy($_POST['session_id']);
+				break;
+
+			case 'session_get_member':
+				$response = $this->api_session_get_member($_POST['session_id']);
+				break;
+
+			case 'session_test_committee':
+				$response = $this->api_session_test_committee($_POST['session_id'], $_POST['committee'])
 				break;
 
 			default:
---
+++ include/models/DataModelSession.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once('data/DataModel.php');
+
+/**
+  * A class implementing news data
+  */
+class DataModelSession extends DataModel
+{
+	public function __constructor($db)
+	{
+		parent::__constructor($db, 'sessions', 'session_id');
+	}
+
+	public function create($member_id)
+	{
+		$session_id = sha1(uniqid('session', true));
+
+		$data = array(
+			'session_id' => $session_id,
+			'member_id' => (int) $member_id,
+			'created_on' => date('Y-m-d H:i:s')
+		);
+
+		$iter = new DataIter($this, -1, $data);
+
+		$this->insert($iter, false);
+
+		return $data;
+	}
+
+	public function destroy($session_id)
+	{
+		$data = compact('session_id');
+
+		$iter = new DataIter($this, -1, $data);
+
+		return $this->delete($iter);
+	}
+}
