24cb3247917920ae208d08a2fb961a798eda0fa2
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2014-01-13 23:00:52 +0100

    Oh dear, loggin on in live environment broken :( Quick fix

--- include/login.php
+++ include/login.php
@@ -80,11 +80,11 @@
 		$cookie_time = time() + 24 * 3600 * 31 * 12;
 
 		// Determine the host name for the cookie (try to be as broad as possible so sd.svcover.nl can profit from it)
-		if (preg_match('/([^.]+)\.(?:[a-z\.]{2,6})$/i', $_SERVER['HTTP_HOST'], $match))
-			$domain = $match[1];
-		else if ($_SERVER['HTTP_HOST'] != 'localhost')
-			$domain = $_SERVER['HTTP_HOST'];
-		else
+		// if (preg_match('/([^.]+)\.(?:[a-z\.]{2,6})$/i', $_SERVER['HTTP_HOST'], $match))
+		// 	$domain = $match[1];
+		// else if ($_SERVER['HTTP_HOST'] != 'localhost')
+		// 	$domain = $_SERVER['HTTP_HOST'];
+		// else
 			$domain = null;
 
 		setcookie('cover_session_id',
