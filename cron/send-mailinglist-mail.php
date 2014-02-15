c65c414ed4bb6a457675c2ee8ec9fb1796427438
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2014-02-15 14:09:34 +0100

    Obtain mailing list adres from the message itself.

--- cron/send-mailinglist-mail.php
+++ cron/send-mailinglist-mail.php
@@ -9,20 +9,25 @@ ini_set('display_errors', true);
 
 $mailinglijsten_model = get_model('DataModelMailinglijst');
 
-$lijst = $mailinglijsten_model->get_lijst($argv[1]);
+$message = file_get_contents('php://stdin');
 
-if (!$lijst) {
-	echo "Lijst niet gevonden\n";
-	return -1;
+if (!preg_match('/^Envelope-to: (.+?)$/m', $message, $match)) {
+	echo "Kan envelope-to header niet vinden.\n";
+	return -2;
 }
 
-$message = file_get_contents('php://stdin');
+$lijst = $mailinglijsten_model->get_lijst($match[1]);
+
+if (!$lijst) {
+	echo "Lijst {$match[1]} niet gevonden\n";
+	return -3;
+}
 
 $aanmeldingen = $mailinglijsten_model->get_aanmeldingen($lijst->get('id'));
 
 foreach ($aanmeldingen as $aanmelding)
 {
-	echo "Sending mail to " . $aanmelding->get('naam') . " <" . $aanmelding->get('email') . ">\n";
+	echo "Sending mail to " . $aanmelding->get('naam') . " <" . $aanmelding->get('email') . ">: ";
 	$descriptors = array(
 		0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
 		1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
@@ -43,16 +48,14 @@ foreach ($aanmeldingen as $aanmelding)
 	fclose($pipes[0]);
 
 	// Read the stdout
-	echo "  out: " . stream_get_contents($pipes[1]) . "\n";
+	// echo "  out: " . stream_get_contents($pipes[1]) . "\n";
 	fclose($pipes[1]);
 
 	// Read the stderr 
-	echo "  err: " . stream_get_contents($pipes[2]) . "\n";
+	// echo "  err: " . stream_get_contents($pipes[2]) . "\n";
 	fclose($pipes[2]);
 
-	$return_value = proc_close($sendmail);
-	echo "  return value: $return_value\n";
-
+	echo proc_close($sendmail);
 	echo "\n";
 }
 
