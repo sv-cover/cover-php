sv-cover/cover-php

commit b73a57addd4d3d16a6b96db0efb2a1f293ab56d7

Let member_full_name, used throughout the site, respect a user his privacy preferences.

include/models/DataModelActieveLeden.php
@@ -16,7 +16,8 @@
 			$rows = $this->db->query('SELECT id,
 					voornaam,
 					tussenvoegsel,
-					achternaam
+					achternaam,
+					privacy,
 					FROM leden
 					WHERE (voornaam ILIKE \'%' . $this->escape_string($query) . '%\' OR
 					achternaam ILIKE \'%' . $this->escape_string($query) . '%\')
@@ -38,6 +39,7 @@
 				l.tussenvoegsel,
 				l.achternaam,
 				l.email,
+				l.privacy,
 				COUNT(a.commissieid) as commissie_count
 				FROM actieveleden a
 				LEFT JOIN leden l ON a.lidid = l.id

include/member.php
@@ -53,18 +53,40 @@
 	  * of the currently logged in one
 	  * @result the currently logged in members full name
 	  */
-	function member_full_name($iter = null) {
+	function member_full_name($iter = null, $override_privacy = false)
+	{
+		$model = get_model('DataModelMember');
+
 		if ($iter) {
-			if (is_numeric($iter)) {
-				$model = get_model('DataModelMember');
+			// If the iter is just a member id, fetch that member is data.
+			if (is_numeric($iter))
 				$iter = $model->get_iter($iter);
-			}
-			
-			$member_data = $iter->data;
-		} elseif (!($member_data = logged_in())) {
-			return __('Geen naam');
+
+			$is_self = ($data = logged_in()) && $data['lidid'] == $iter->get('lidid');
+		}
+		// No argument provided, get the full name of the currently logged in member.
+		else {
+			$iter = ($data = logged_in())
+				? new DataIter($model, $data['lidid'], $data)
+				: null;
+
+			$is_self = true;
 		}
 
-		return $member_data['voornaam'] . ($member_data['tussenvoegsel'] ? (' ' . $member_data['tussenvoegsel']) : '') . ' ' . $member_data['achternaam'];
+		// When the user is not found (or not logged in)
+		if (!$iter)
+			return __('Geen naam');
+
+		// Or when the privacy settings prevent their name from being displayed
+		if (!$override_privacy
+			&& $model->is_private($iter, 'naam')
+			&& !$is_self
+			&& !member_in_commissie(COMMISSIE_BESTUUR))
+			return __('Onbekend');
+
+		// Construct a member his full name
+		return $iter->get('voornaam')
+			 . ($iter->get('tussenvoegsel') ? ' ' . $iter->get('tussenvoegsel') : '')
+			 . ' ' . $iter->get('achternaam');
 	}
 ?>

include/models/DataModelCommissie.php
@@ -54,6 +54,7 @@
 					leden.tussenvoegsel, 
 					leden.achternaam, 
 					leden.email, 
+					leden.privacy,
 					actieveleden.functie,
 					actieveleden.sleutel,
 					actieveleden.id AS actiefid

include/models/DataModelMember.php
@@ -65,6 +65,7 @@
 						voornaam,
 						tussenvoegsel,
 						achternaam,
+						privacy,
 						(EXTRACT(YEAR FROM CURRENT_TIMESTAMP) - EXTRACT(YEAR FROM geboortedatum)) AS leeftijd
 					FROM
 						leden

profiel.php
@@ -27,7 +27,12 @@
 		}
 		
 		function get_content($view, $iter = null, $params = null) {
-			$this->run_header(array('title' => __('Profiel')));
+
+			$title = $iter
+				? member_full_name($iter)
+				: __('Profiel');
+
+			$this->run_header(compact('title'));
 			run_view('profiel::' . $view, $this->model, $iter, $params);
 			$this->run_footer();
 		}

themes/default/views/boeken/boeken.php
@@ -342,7 +342,7 @@
 
 			foreach ($iters as $iter) {
 				$total = money_format('%.2n', $this->totaalprijs($model, $iter->get('id'), $nnb));
-				$csv .= csv_row(array(member_full_name($iter), $iter->get('aantal_bestellingen'), $total . ($nnb ? '*' : ''))) . "\n";
+				$csv .= csv_row(array(member_full_name($iter, true), $iter->get('aantal_bestellingen'), $total . ($nnb ? '*' : ''))) . "\n";
 			}
 		
 			header('Content-Description: File Transfer');
@@ -371,7 +371,7 @@
 				$class = 'r' . ($i ? '0' : '1');
 			
 				echo '<tr class="' . $class . '">
-					<td>' . htmlspecialchars(member_full_name($iter)) . '</td>';
+					<td>' . htmlspecialchars(member_full_name($iter, true)) . '</td>';
 			
 				echo '<td class="text_right">' . $iter->get('aantal_bestellingen') . '</td>';
 			
@@ -413,7 +413,7 @@
 				echo '<tr class="' . $class . '">
 					<td>' . htmlspecialchars($iter->get('titel')) . '</td>';
 			
-				echo '<td>' . htmlspecialchars(member_full_name($iter)) . '</td>
+				echo '<td>' . htmlspecialchars(member_full_name($iter, true)) . '</td>
 				<td class="text_right">'. ($iter->get('prijs') == 0 ? 'n.n.b.' : ('&euro; ' . money_format('%.2n', $iter->get('prijs')))) . '</td>
 				</tr>';
 			
@@ -428,7 +428,7 @@
 			$csv = csv_row(array(__('Boek'), __('Lid'), __('Prijs'))) . "\n";
 
 			foreach ($iters as $iter)			
-				$csv .= csv_row(array($iter->get('titel'), member_full_name($iter), $iter->get('prijs') == 0 ? 'n.n.b.' : money_format('%.2n', $iter->get('prijs')))) . "\n";
+				$csv .= csv_row(array($iter->get('titel'), member_full_name($iter, true), $iter->get('prijs') == 0 ? 'n.n.b.' : money_format('%.2n', $iter->get('prijs')))) . "\n";
 		
 			header('Content-Description: File Transfer');
 			header('Content-Type: application/force-download');

themes/default/views/profiel/profiel.phtml
@@ -1,4 +1,4 @@
-<h1><?= sprintf(__('Profiel van %s'), $this->get_privacy_name($model, $iter)) ?></h1>
+<h1><?= sprintf(__('Profiel van %s'), member_full_name($iter)) ?></h1>
 <div class="messageBox clearfix">
 
 

commit 2f5fb1e0b2dd3bbfd6e65d85a5c17315ef9584a2

Fix an issue with the active members admin list caused by b73a57a.

include/models/DataModelActieveLeden.php
@@ -43,7 +43,7 @@
 				COUNT(a.commissieid) as commissie_count
 				FROM actieveleden a
 				LEFT JOIN leden l ON a.lidid = l.id
-				GROUP BY l.id, l.voornaam, l.tussenvoegsel, l.achternaam, l.email
+				GROUP BY l.id, l.voornaam, l.tussenvoegsel, l.achternaam, l.email, l.privacy
 				ORDER BY voornaam, tussenvoegsel, achternaam ASC');
 
 			return $this->_rows_to_iters($rows);