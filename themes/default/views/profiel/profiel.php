54f287ba491813f70d57023029e8f0753d0eb7e3
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2014-05-16 00:05:05 +0200

    Missed even more calls :(

--- themes/default/views/profiel/profiel.php
+++ themes/default/views/profiel/profiel.php
@@ -37,7 +37,7 @@ class ProfielView extends View {
 			/* If the currently logged in member does not have write access 
 			 * and the field is private, then return ''
 			 */
-			if (!$this->member_write_permission($iter->get('lidid')) && $model->is_private($iter, $name))
+			if (!$this->member_write_permission($iter) && $model->is_private($iter, $name))
 				return '';
 
 			/* Setup the label */
@@ -45,7 +45,7 @@ class ProfielView extends View {
 
 			/* Show an input text field when there is write permission and
 			 * there is data */
-			if (member_in_commissie(COMMISSIE_BESTUUR) || ($this->member_write_permission($iter->get('lidid')) && !$read_only))
+			if (member_in_commissie(COMMISSIE_BESTUUR) || ($this->member_write_permission($iter) && !$read_only))
 				$result .= input_text($name, $data);
 			else /* Show the field otherwise */
 				$result .= htmlspecialchars($data[$name]);
@@ -55,19 +55,11 @@ class ProfielView extends View {
 				if (strtolower($data['woonplaats']) == 'groningen')
 					$provincie = 'Groningen';
 
-				$result .= ' <a href="http://maps.google.nl/maps?f=q&hl=nl&q=' . urlencode($data['adres'] . ', ' . $provincie . ' ' . $data['postcode'] . ' ' . $data['woonplaats']) . '&ie=UTF8&z=15&om=1&iwloc=addr">' . __('opzoeken') . '</a>';
+				$result .= ' <a href="http://maps.google.nl/maps?f=q&hl=nl&q=' . rawurlencode($data['adres'] . ', ' . $provincie . ' ' . $data['postcode'] . ' ' . $data['woonplaats']) . '&ie=UTF8&z=15&om=1&iwloc=addr">' . __('opzoeken') . '</a>';
 
 			}
 
 			return $result . '</td></tr>';
-		}
-
-
-		function get_privacy_name($model, $iter) {
-			if ($model->is_private($iter, 'naam') && !$this->member_write_permission($iter->get('id')))
-				return __('Onbekend');
-			else
-				return $iter->get('nick') ? $iter->get('nick') : member_full_name($iter);
 		}
 
 		public function get_active_session_count($member_id)
