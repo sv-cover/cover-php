b4a8db8f46f768e0116ae8abada3fd5fc18b72f7
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2014-08-11 12:25:40 +0200

    Disable book ordering code and refer instead to the boekcie webshop of study store

--- boeken.php
+++ boeken.php
@@ -24,262 +24,262 @@
 			$this->run_footer();
 		}
 		
-		function _page_prepare($id = null, $in_commissie = true) {
-			if (!logged_in()) {
-				$this->get_content('auth');
-				return false;
-			}
+		// function _page_prepare($id = null, $in_commissie = true) {
+		// 	if (!logged_in()) {
+		// 		$this->get_content('auth');
+		// 		return false;
+		// 	}
 	
-			if ($in_commissie && !member_in_commissie(COMMISSIE_BOEKCIE)) {
-				$this->get_content('boekcie');
-				return false;
-			}
+		// 	if ($in_commissie && !member_in_commissie(COMMISSIE_BOEKCIE)) {
+		// 		$this->get_content('boekcie');
+		// 		return false;
+		// 	}
 			
-			if ($id === null)
-				return true;
+		// 	if ($id === null)
+		// 		return true;
 			
-			if (!($iter = $this->model->get_iter($id))) {
-				$this->get_content('not_found');
-				return false;
-			}
+		// 	if (!($iter = $this->model->get_iter($id))) {
+		// 		$this->get_content('not_found');
+		// 		return false;
+		// 	}
 			
-			return $iter;
-		}
+		// 	return $iter;
+		// }
 		
-		function _process_add() {
-			if (!$this->_page_prepare())
-				return;
+		// function _process_add() {
+		// 	if (!$this->_page_prepare())
+		// 		return;
 			
-			$check = array('vak', 
-					'titel', 
-					'auteur', 
-					array('name' => 'prijs', 'function' => 'check_value_tofloat'),
-					array('name' => 'categorie', 'function' => 'check_value_toint'));
+		// 	$check = array('vak', 
+		// 			'titel', 
+		// 			'auteur', 
+		// 			array('name' => 'prijs', 'function' => 'check_value_tofloat'),
+		// 			array('name' => 'categorie', 'function' => 'check_value_toint'));
 			
-			$data = check_values($check, $errors);
+		// 	$data = check_values($check, $errors);
 			
-			if (count($errors) > 0) {
-				$this->get_content('boeken', null, array('errors' => $errors));
-				return;
-			}
+		// 	if (count($errors) > 0) {
+		// 		$this->get_content('boeken', null, array('errors' => $errors));
+		// 		return;
+		// 	}
 			
-			$data['status'] = 1;
+		// 	$data['status'] = 1;
 
-			$iter = new DataIter($this->model, -1, $data);
-			$this->model->insert($iter);
+		// 	$iter = new DataIter($this->model, -1, $data);
+		// 	$this->model->insert($iter);
 			
-			header('Location: ' . add_request(get_request(), 'added=yes'));
-		}
+		// 	header('Location: ' . add_request(get_request(), 'added=yes'));
+		// }
 		
-		function _process_edit() {
-			if ($this->_page_prepare() === false)
-				return;
+		// function _process_edit() {
+		// 	if ($this->_page_prepare() === false)
+		// 		return;
 			
-			$all_errors = array();
-			$deletes = array();
+		// 	$all_errors = array();
+		// 	$deletes = array();
 
-			foreach ($_POST as $field => $value) {
-				if (strncmp($field, "id_", 3) != 0)
-					continue;
+		// 	foreach ($_POST as $field => $value) {
+		// 		if (strncmp($field, "id_", 3) != 0)
+		// 			continue;
 				
-				$id = substr($field, 3);
-				$iter = $this->model->get_iter($id);
+		// 		$id = substr($field, 3);
+		// 		$iter = $this->model->get_iter($id);
 				
-				if (!$iter)
-					continue;
+		// 		if (!$iter)
+		// 			continue;
 					
-				if (get_post('del_' . $id) == 'yes') {
-					if ($this->model->num_bestellingen($iter) > 0) {
-						/* This book still has orders,
-						 * inform the user
-						 */
-						$deletes[] = $iter;
-						continue;
-					} else {
-						/* Delete this book now */
-						$this->model->delete($iter);
-						continue;
-					}
-				}
+		// 		if (get_post('del_' . $id) == 'yes') {
+		// 			if ($this->model->num_bestellingen($iter) > 0) {
+		// 				/* This book still has orders,
+		// 				 * inform the user
+		// 				 */
+		// 				$deletes[] = $iter;
+		// 				continue;
+		// 			} else {
+		// 				/* Delete this book now */
+		// 				$this->model->delete($iter);
+		// 				continue;
+		// 			}
+		// 		}
 				
-				if (get_post('suspend_' . $id) == 'yes' && $iter->get('status')) {
-					/* Mark the status as suspended */
-					$iter->set('status', 0);
-					$this->model->update($iter);
-				} elseif (get_post('suspend_' . $id) != 'yes' && !$iter->get('status')) {
-					$iter->set('status', 1);
-					$this->model->update($iter);
-				}
+		// 		if (get_post('suspend_' . $id) == 'yes' && $iter->get('status')) {
+		// 			/* Mark the status as suspended */
+		// 			$iter->set('status', 0);
+		// 			$this->model->update($iter);
+		// 		} elseif (get_post('suspend_' . $id) != 'yes' && !$iter->get('status')) {
+		// 			$iter->set('status', 1);
+		// 			$this->model->update($iter);
+		// 		}
 
-				$check = array('vak_' . $id, 
-						'titel_' . $id, 
-						'auteur_' . $id, 
-						array('name' => 'prijs_' . $id, 'function' => 'check_value_tofloat'));
+		// 		$check = array('vak_' . $id, 
+		// 				'titel_' . $id, 
+		// 				'auteur_' . $id, 
+		// 				array('name' => 'prijs_' . $id, 'function' => 'check_value_tofloat'));
 			
-				$data = check_values($check, $errors);
+		// 		$data = check_values($check, $errors);
 
-				if (count($errors) > 0) {
-					$all_errors += $errors;
-					continue;
-				}
+		// 		if (count($errors) > 0) {
+		// 			$all_errors += $errors;
+		// 			continue;
+		// 		}
 			
-				$iter->set('vak', $data['vak_' . $id]);
-				$iter->set('titel', $data['titel_' . $id]);
-				$iter->set('auteur', $data['auteur_' . $id]);
-				$iter->set('prijs', $data['prijs_' . $id]);
+		// 		$iter->set('vak', $data['vak_' . $id]);
+		// 		$iter->set('titel', $data['titel_' . $id]);
+		// 		$iter->set('auteur', $data['auteur_' . $id]);
+		// 		$iter->set('prijs', $data['prijs_' . $id]);
 				
-				$this->model->update($iter);
-			}
+		// 		$this->model->update($iter);
+		// 	}
 			
-			/* Globale bestel status opslaan */
-			$configuratie = get_model('DataModelConfiguratie');
+		// 	/* Globale bestel status opslaan */
+		// 	$configuratie = get_model('DataModelConfiguratie');
 			
-			$iter = new DataIter($configuratie, 'boeken_bestellen', array());
-			$iter->set('value', get_post('vastzetten') == 'yes' ? '0' : '1');
+		// 	$iter = new DataIter($configuratie, 'boeken_bestellen', array());
+		// 	$iter->set('value', get_post('vastzetten') == 'yes' ? '0' : '1');
 
-			$configuratie->update($iter);
+		// 	$configuratie->update($iter);
 			
-			if (count($deletes) > 0) {
-				$this->get_content('delete', $deletes);
-			} elseif (count($all_errors) > 0) {
-				$this->get_content('boeken', null, array('errors' => $all_errors));
-			} else {
-				header('Location: ' . get_request());
-			}
-		}
+		// 	if (count($deletes) > 0) {
+		// 		$this->get_content('delete', $deletes);
+		// 	} elseif (count($all_errors) > 0) {
+		// 		$this->get_content('boeken', null, array('errors' => $all_errors));
+		// 	} else {
+		// 		header('Location: ' . get_request());
+		// 	}
+		// }
 		
-		function _process_bestel() {
-			if ($this->_page_prepare(null, false) === false)
-				return;
+		// function _process_bestel() {
+		// 	if ($this->_page_prepare(null, false) === false)
+		// 		return;
 			
-			$configuratie = get_model('DataModelConfiguratie');
-			$bestellen = $configuratie->get_value('boeken_bestellen');
+		// 	$configuratie = get_model('DataModelConfiguratie');
+		// 	$bestellen = $configuratie->get_value('boeken_bestellen');
 			
-			if (!$bestellen) {
-				$this->get_content('deadline', null, null, false);
-				return;
-			}
+		// 	if (!$bestellen) {
+		// 		$this->get_content('deadline', null, null, false);
+		// 		return;
+		// 	}
 			
-			$member_data = logged_in();
+		// 	$member_data = logged_in();
 			
-			foreach ($_POST as $field => $value) {
-				if (strncmp($field, "boek_", 5) != 0)
-					continue;
+		// 	foreach ($_POST as $field => $value) {
+		// 		if (strncmp($field, "boek_", 5) != 0)
+		// 			continue;
 				
-				$id = substr($field, 5);
-				$iter = $this->model->get_iter($id);
+		// 		$id = substr($field, 5);
+		// 		$iter = $this->model->get_iter($id);
 				
-				if (!$iter)
-					continue;
+		// 		if (!$iter)
+		// 			continue;
 				
-				$bestelling = new DataIter($this->model, -1, 
-						array('lidid' => intval($member_data['id']),
-						'boekid' => intval($iter->get_id())));
+		// 		$bestelling = new DataIter($this->model, -1, 
+		// 				array('lidid' => intval($member_data['id']),
+		// 				'boekid' => intval($iter->get_id())));
 					
-				$this->model->insert_bestelling($bestelling);
-			}
+		// 		$this->model->insert_bestelling($bestelling);
+		// 	}
 			
-			header('Location: ' . get_request());
-		}
+		// 	header('Location: ' . get_request());
+		// }
 		
-		function _process_unbestel() {
-			if ($this->_page_prepare(null, false) === false)
-				return;
+		// function _process_unbestel() {
+		// 	if ($this->_page_prepare(null, false) === false)
+		// 		return;
 
-			$member_data = logged_in();
+		// 	$member_data = logged_in();
 
-			foreach ($_POST as $field => $value) {
-				if (strncmp($field, 'id_', 3) != 0)
-					continue;
+		// 	foreach ($_POST as $field => $value) {
+		// 		if (strncmp($field, 'id_', 3) != 0)
+		// 			continue;
 				
-				$id = substr($field, 3);
-				$iter = $this->model->get_iter($id);
+		// 		$id = substr($field, 3);
+		// 		$iter = $this->model->get_iter($id);
 				
-				if (!$iter)
-					continue;
+		// 		if (!$iter)
+		// 			continue;
 				
-				$bestelling = $this->model->get_bestelling($iter, $member_data['id']);
+		// 		$bestelling = $this->model->get_bestelling($iter, $member_data['id']);
 
-				if (!$bestelling)
-					continue;
+		// 		if (!$bestelling)
+		// 			continue;
 
-				$this->model->delete_bestelling($bestelling);
-			}
+		// 		$this->model->delete_bestelling($bestelling);
+		// 	}
 			
-			header('Location: ' . get_request());
-		}
+		// 	header('Location: ' . get_request());
+		// }
 		
-		function _process_delete_bestellingen() {
-			if ($this->_page_prepare() === false)
-				return;
+		// function _process_delete_bestellingen() {
+		// 	if ($this->_page_prepare() === false)
+		// 		return;
 			
-			$this->model->delete_bestellingen();
-			header('Location: ' . get_request());
-		}
+		// 	$this->model->delete_bestellingen();
+		// 	header('Location: ' . get_request());
+		// }
 		
-		function _show_bestellingen() {
-			if (!member_in_commissie(COMMISSIE_BOEKCIE)) {
-				$this->get_content('boekcie', null, null, false);
-				return;
-			}
+		// function _show_bestellingen() {
+		// 	if (!member_in_commissie(COMMISSIE_BOEKCIE)) {
+		// 		$this->get_content('boekcie', null, null, false);
+		// 		return;
+		// 	}
 			
-			if (isset($_GET['order_by'])) {
-				$order = $_GET['order_by'];
+		// 	if (isset($_GET['order_by'])) {
+		// 		$order = $_GET['order_by'];
 				
-				if (!($order == 'boek' || $order == 'lid' || $order == 'prijs'))
-					$order = 'boek';
-			} else {
-				$order = 'boek';
-			}
+		// 		if (!($order == 'boek' || $order == 'lid' || $order == 'prijs'))
+		// 			$order = 'boek';
+		// 	} else {
+		// 		$order = 'boek';
+		// 	}
 			
-			$params = array('order_by' => $order);
+		// 	$params = array('order_by' => $order);
 			
-			if (isset($_GET['method'])) {
-				$method = $_GET['method'];
+		// 	if (isset($_GET['method'])) {
+		// 		$method = $_GET['method'];
 
-				if (!isset($_GET['type'])) {
-					$view = 'bestellingen_' . $method;
-					$iters = $this->model->get_bestellingen($order);
-				} else {
-					if ($_GET['type'] == 'group_by_book') {
-						$view = 'bestellingen_' . $method . '_by_book';
-						$iters = $this->model->get_by_book();
-					} elseif ($_GET['type'] == 'group_by_member') {
-						$view = 'bestellingen_' . $method . '_by_member';
-						$iters = $this->model->get_by_member();
-					} else {
-						$view = 'bestellingen_' . $method;
-						$iters = $this->model->get_bestellingen($order); 
-					}
-				}
+		// 		if (!isset($_GET['type'])) {
+		// 			$view = 'bestellingen_' . $method;
+		// 			$iters = $this->model->get_bestellingen($order);
+		// 		} else {
+		// 			if ($_GET['type'] == 'group_by_book') {
+		// 				$view = 'bestellingen_' . $method . '_by_book';
+		// 				$iters = $this->model->get_by_book();
+		// 			} elseif ($_GET['type'] == 'group_by_member') {
+		// 				$view = 'bestellingen_' . $method . '_by_member';
+		// 				$iters = $this->model->get_by_member();
+		// 			} else {
+		// 				$view = 'bestellingen_' . $method;
+		// 				$iters = $this->model->get_bestellingen($order); 
+		// 			}
+		// 		}
 
 				
-				run_view('boeken::' . $view, $this->model, $iters, $params);
-			} else {
-				$this->get_content('bestellingen', $this->model->get_bestellingen($order), $params, false);	
-			}
-		}
+		// 		run_view('boeken::' . $view, $this->model, $iters, $params);
+		// 	} else {
+		// 		$this->get_content('bestellingen', $this->model->get_bestellingen($order), $params, false);	
+		// 	}
+		// }
 		
 		function run_impl() {
-			if (!logged_in())
-				$this->get_content('auth');
-			elseif (isset($_POST['submboekenadd']))
-				$this->_process_add();
-			elseif (isset($_POST['submboekenedit']))
-				$this->_process_edit();
-			elseif (isset($_POST['submboekenbestel']))
-				$this->_process_bestel();
-			elseif (isset($_POST['submboekenunbestel']))
-				$this->_process_unbestel();
-			elseif (isset($_POST['submboekendelbestellingen']))
-				$this->_process_delete_bestellingen();
-			elseif (isset($_GET['bestellingen']))
-				$this->_show_bestellingen();
-			else
-				$this->get_content('boeken', null, isset($_GET['added']) ? array('added' => true) : null);
+			// if (!logged_in())
+			// 	$this->get_content('auth');
+			// elseif (isset($_POST['submboekenadd']))
+			// 	$this->_process_add();
+			// elseif (isset($_POST['submboekenedit']))
+			// 	$this->_process_edit();
+			// elseif (isset($_POST['submboekenbestel']))
+			// 	$this->_process_bestel();
+			// elseif (isset($_POST['submboekenunbestel']))
+			// 	$this->_process_unbestel();
+			// elseif (isset($_POST['submboekendelbestellingen']))
+			// 	$this->_process_delete_bestellingen();
+			// elseif (isset($_GET['bestellingen']))
+			// 	$this->_show_bestellingen();
+			// else
+			// 	$this->get_content('boeken', null, isset($_GET['added']) ? array('added' => true) : null);
+			$this->get_content('go_to_webshop');
 		}
 	}
 	
 	$controller = new ControllerBoeken();
 	$controller->run();
-?>
