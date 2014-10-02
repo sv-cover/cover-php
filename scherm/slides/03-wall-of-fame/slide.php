c3a76d5aa63754df8effc3b89fb238dae58c6af7
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2014-10-02 09:48:18 +0200

    Make the 'looking for new members' banner a bit larger.

--- scherm/slides/03-wall-of-fame/slide.php
+++ scherm/slides/03-wall-of-fame/slide.php
@@ -31,9 +31,9 @@ function _full_name($lid) {
 	</div>
 	<?php endforeach ?>
 	<?php if ($commissie->get('vacancies')): ?>
-		<div style="background: #c60c30; color: white; line-height: 32px; position: absolute; bottom: 0; left: 0; right: 0; text-align:center; font-size: 20px;">
+		<div style="background: #c60c30; color: white; line-height: 64px; position: absolute; bottom: 0; left: 0; right: 0; text-align:center; font-size: 40px;">
 			<?php if ($commissioner = $commissie_model->get_lid_for_functie(COMMISSIE_BESTUUR, 'commissaris intern')): ?>
-				<img src="foto.php?lid_id=<?=$commissioner->get('id')?>&amp;get_thumb=circle&amp;width=100" width="50" height="50" style="border-radius: 50% float: left; margin: 7px 7px 0 0;">
+				<img src="foto.php?lid_id=<?=$commissioner->get('id')?>&amp;get_thumb=circle&amp;width=100" width="100" height="100" style="border-radius: 50% float: left; margin: 14px 14px 0 0;">
 				<div style="display: inline-block; text-align:left"><?=sprintf(__('Hey! Deze commissie zoekt nieuwe commissieleden.<br>Heb jij interesse? Zeg het de Commissaris Intern, %s (intern@svcover.nl)'), markup_format_text(member_full_name($commissioner))) ?></div>
 			<?php else: ?>
 				<div style="display: inline-block; text-align:left"><?=__('Hey! Deze commissie zoekt nieuwe commissieleden. Heb jij interesse? Zeg het de Intern via intern@svcover.nl.') ?></div>
