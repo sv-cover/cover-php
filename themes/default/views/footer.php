08fe43557993211552b319481496013cca83391d
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2014-10-18 22:53:38 +0200

    Banners are always 120px width and open in a new window.

--- themes/default/views/footer.php
+++ themes/default/views/footer.php
@@ -19,7 +19,7 @@
                         $banners = $rotator -> get(7);
                         foreach ($banners as $banner)
                         {
-                            echo '<a href="'.$banner['url'].'"><img src="images/banners/'.$banner['filename'].'" /></a><br /><br />';
+                            echo '<a href="'.$banner['url'].'" target="_new"><img src="images/banners/'.$banner['filename'].'"></a><br /><br />';
                         }
                     ?>
 				</div>
