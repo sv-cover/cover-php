7d0ec601b1155fdf73f0074c654134a68b299383
Martijn Luinstra <martijnluinstra@gmail.com>
2014-10-29 00:14:58 +0100

    Replace the buggy photo-layout algorithm by the one used in the photo albums of the website.

--- scherm/slides/01-fotos/slide.php
+++ scherm/slides/01-fotos/slide.php
@@ -6,19 +6,17 @@ $boek = $model->get_random_book();
 
 $fotos = $model->get_photos($boek,30,true);
 
-// Willekeurige volgorde!
+
 ?>
 <div class="collage">
 	<h1><?=$boek->get('titel')?></h1>
-	<ul class="collage">
+	<ul class="flow-gallery">
 		<? foreach ($fotos as $foto): ?>
 		<li class="foto">
-			<figure>
-				<img src="<?=$foto->get('url')?>">
-				<figcaption><?=markup_format_text($foto->get('titel'))?></figcaption>
-			</figure>
+			<img src="<?=$foto->get('url')?>" height="<?=$foto->get('height')?>" width="<?=$foto->get('width')?>">
+			<span class="description"><?=markup_format_text($foto->get('beschrijving'))?></span>
 		</li>
 		<? endforeach ?>
 	</ul>
 </div>
-<script>layout_photos(this);</script>
+<script>layout_photos();</script>
