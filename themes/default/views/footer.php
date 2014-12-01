1a7ce0cc00cf085cac83cb886775c3b15b9ab146
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2014-12-01 12:21:16 +0100

    Update Google Analytics tracking code

--- themes/default/views/footer.php
+++ themes/default/views/footer.php
@@ -31,7 +31,8 @@
 		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		  ga('create', 'UA-12500955-1', 'svcover.nl');
+		  ga('create', 'UA-12500955-1', 'auto');
+		  ga('require', 'displayfeatures');
 		  ga('send', 'pageview');
 
 		</script>
