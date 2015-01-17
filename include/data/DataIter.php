3ae9d198c2224eee0ffdc730c78a5e1c694305a1
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-01-17 21:36:22 +0100

    Redefine DataIter::has as a test to return if that DataIter has that property, not whether it is empty or not.

--- include/data/DataIter.php
+++ include/data/DataIter.php
@@ -43,8 +43,7 @@
 		}
 
 		public function has($field) {
-			// return array_key_exists($this->namespace . $field, $this->data);
-			return isset($this->data[$this->namespace . $field]);
+			return array_key_exists($this->namespace . $field, $this->data);
 		}
 		
 		/**
