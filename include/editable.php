5f11fd89cda0192a0608a0916fbc99638c388c89
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-01-17 12:13:08 +0100

    Remove deprecated committee markup code

--- include/editable.php
+++ include/editable.php
@@ -4,32 +4,18 @@
 
 	require_once 'include/markup.php';
 	require_once 'include/member.php';
-
-	function _editable_parse_deprecated(&$page) {
-		$page = str_ireplace(array(
-				'[commissie_leden]',
-				'[commissie_poll]',
-				'[commissie_email]',
-				'[commissie_foto]',
-				'[commissie_agenda]'), '', $page);
-	}
 	
 	function _editable_parse_commissie_prive(&$page, $owner) {
 		if (member_in_commissie($owner))
-			$page = preg_replace(Array('/\[prive\]/i','/\[\/prive\]/i'), '', $page);
+			$page = preg_replace(array('/\[prive\]/i','/\[\/prive\]/i'), '', $page);
 		else
-			$page = preg_replace('/\[prive\](.*?)\[\/prive\]/ism', '', $page);
+			$page = preg_replace('/\[prive\].*?\[\/prive\]/ism', '', $page);
 			
 	}
 	
 	function _editable_parse_commissie_summary(&$page, $owner) {
 		/* Just remove because the summary isn't used in general view */
 		$page = preg_replace('/\[samenvatting\](.+?)\[\/samenvatting\]\s*/ism', '', $page);
-	}
-
-	function _editable_parse_commissie_header(&$page, $owner) {
-		/* Just remove because the header isn't used in general view */
-		$page = preg_replace('/\[h1\](.+?)\[\/h1\]\s*/ism', '', $page);
 	}
 
 	/** @group Editable
@@ -43,14 +29,12 @@
 	function editable_parse($page, $owner) {
 		$placeholders = array();
 
-		_editable_parse_deprecated($page);
-		_editable_parse_commissie_header($page, $owner);
 		_editable_parse_commissie_summary($page, $owner);
-
-		$page = markup_parse($page);
 
 		_editable_parse_commissie_prive($page, $owner);
 		
+		$page = markup_parse($page);
+
 		return markup_clean($page);
 	}
 	
