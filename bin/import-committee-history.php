e3065118d0dc12c633dfe7b62a27a3046b7fd041
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-03-29 11:19:44 +0200

    Add support for committee membership xls file (once exported to tsv)

--- bin/import-committee-history.php
+++ bin/import-committee-history.php
@@ -47,6 +47,7 @@ $commissienamen = array(
 	
 	'AlmanacCee' => 4,
 	'AlmanakCie' => 4,
+	'AlamanakCie' => 4,
 
 	'EerstejaarsCie' => 17,
 	
@@ -104,6 +105,7 @@ $commissienamen = array(
 
 	'PubliciTee' => 24,
 	'Promotie' => 24,
+	'Promitie' => 24,
 
 	'SporTee' => 20,
 	'Conditie' => 20,
@@ -114,7 +116,9 @@ $commissienamen = array(
 	'StudCee' => 31,
 	'Study Support Committee' => 31,
 
-	'WebCie' => 1
+	'WebCie' => 1, 
+
+	'Bestuur' => 0
 );
 
 $commissies = array();
@@ -136,98 +140,165 @@ foreach ($member_iter as $iter)
 		$members[(int) $iter->get_id()] = array('pattern' => $name_pattern, 'iter' => $iter);
 	}
 
-echo "Connecting to MySQL\n";
-$pdo = new PDO('mysql:host=127.0.0.1;port=33060;dbname=sd', 'sd', $argv[1]);
-
-echo "Getting documents\n";
-$rows = $pdo->query('SELECT uid, path, keywords FROM files WHERE path LIKE "/home/bestuur/bestuur/standaarddocumenten/Minutes/%" AND path LIKE "%.pdf"');
-
-// $rows = $pdo->query('SELECT uid, path, keywords FROM files WHERE uid = "9a6c13ab47f9ad0961fc73d6c41309c6"');
-
-echo "Magic!\n";
-foreach ($rows as $row)
+function import_from_file($fh)
 {
-	// echo "{$row['uid']}\t{$row['path']}:\n";
+	global $commissies, $members;
 
-	$date = null;
+	$header_1 = fgetcsv($fh, 0, "\t");
+	$header_2 = fgetcsv($fh, 0, "\t");
 
-	if (preg_match('/\/[^\/]*(20\d{2})-?(\d{1,2})-?(\d{1,2})[^\/]+$/', $row['path'], $match))
-		$date = new DateTime(sprintf('%04d-%02d-%02d', $match[1], $match[2], $match[3]));
-	
-	elseif (preg_match('/\/[^\/]*(\d{1,2})-(\d{1,2})-(20\d{2})[^\/]+$/', $row['path'], $match))
-		$date = new DateTime(sprintf('%04d-%02d-%02d', $match[3], $match[2], $match[1]));
-
-	else
-		continue;
-	
-	if (!preg_match_all('/^Besluit(?!en)(.+?)(\s{2,})/ism', $row['keywords'], $matches))
-		continue;
-
-	foreach ($matches[1] as $i => $line)
+	$n_row = 2;
+	while ($row = fgetcsv($fh, 0, "\t"))
 	{
-		$line = preg_replace("/\d+/", " ", $line);
+		$n_row++;
+		$commissielid = null;
 
-		$line = preg_replace("/\s+/", " ", $line);
-
-		$line = trim($line);
-
-		$commissie = null;
-
-		$actie = null;
-
-		$commissieleden = array();
-
-		$coverage = array();
-		
-		if (preg_match('/(uit|in)ge(-\s)?hamerd/i', $line, $match, PREG_OFFSET_CAPTURE)
-			|| preg_match('/(uit|in)\sde\s(.+?)\sge(-\s)?hamerd/', $line, $match, PREG_OFFSET_CAPTURE)
-			|| preg_match('/gaat\sde\s(.+?)\s(uit|in)/', $line, $match, PREG_OFFSET_CAPTURE)) {
-
-			$actie = $match[1][0] . 'gehamerd';
-			$coverage[] = array($match[0][1], strlen($match[0][0]));
+		foreach ($members as $member) {
+			if (preg_match('/^' . $member['pattern'] . '$/i', trim($row[0]))) {
+				$commissielid = $member['iter'];
+				break;
+			}
 		}
-		
-		if (preg_match('/' . implode('|', array_keys($commissies)) . '/i', $line, $match, PREG_OFFSET_CAPTURE)) {
-			$commissie = $commissies[strtolower($match[0][0])];
-			$coverage[] = array($match[0][1], strlen($match[0][0]));
+
+		if (!$commissielid) {
+			echo "SKIP: $n_row:0 Kan geen commissielid vinden in {$row[0]}\n";
+			continue;
 		}
-		
-		foreach ($members as $id => $member) {
-			if ((int) $member['iter']->get('beginjaar') > (int) $date->format('Y'))
+
+		for ($i = 1; $i < count($row); ++$i)
+		{
+			if ($row[$i] == 0)
 				continue;
 
-			if (preg_match('/' . $member['pattern'] . '/is', $line, $match, PREG_OFFSET_CAPTURE)) {
-				$commissieleden[$id] = $member['iter'];
-				$coverage[] = array($match[0][1], strlen($match[0][0]));
+			if (!preg_match('/^(Begin|Eind) (' . implode('|', array_keys($commissies)) . ')$/i', $header_2[$i], $match)) {
+				// echo "SKIP: $n_row:$i geen begin of eind + commissie in {$header_2[$i]}\n";
+				continue;
 			}
-		}
+			elseif (strtolower($match[1]) == 'begin')
+				$actie = 'ingehamerd';
+			elseif (strtolower($match[1]) == 'eind')
+				$actie = 'uitgehamerd';
 
-		if (!empty($commissieleden) && $actie && $commissie) {
-			if ($actie == 'ingehamerd') {
-				foreach ($commissieleden as $commissielid) {
-					$membership_model->start_membership($commissie, $commissielid, null, $date);
-				}
-			}
-			elseif ($actie == 'uitgehamerd') {
-				foreach ($commissieleden as $commissielid) {
-					$membership = $membership_model->find_membership($commissie, $commissielid, $date);
+			$commissie = $commissies[strtolower($match[2])];
 
-					if (!$membership)
-						$membership = $membership_model->start_membership($commissie, $commissielid, null, null);
-					
-					$membership_model->end_membership($membership->get_id(), $date);
-				}
+			if (!preg_match('/^(\d\d)\/(\d\d)\/(\d\d)$/', $row[$i], $match)) {
+				echo "SKIP: $n_row:$i kan datum niet parsen\n";
+				continue;
 			}
+			else {
+				echo sprintf('20%02d-%02d-%02d', $match[3], $match[2], $match[1]), "\n";
+				$date = new DateTime(sprintf('20%02d-%02d-%02d', $match[3], $match[2], $match[1]));
+			}
+
+			commit_membership($actie, $commissie, [$commissielid], $date);
 		}
-		
-		echo "\n" . format_coverage($line, $coverage) . "\n";
-		printf("%s %s %s %s: %s\033[0m\n",
-			!empty($commissieleden) && $actie && $commissie
-				? "\033[42m✓"
-				: "\033[41m✗",
-			$date->format('Y-m-d'),
-			$actie ? $actie : '[NULL]',
-			$commissie ? $commissie->get('naam') : '[NULL]',
-			implode(', ', array_map('member_full_name', $commissieleden)));
 	}
 }
+
+function import_from_sd()
+{
+	global $commissies, $members;
+
+	echo "Connecting to MySQL\n";
+	$pdo = new PDO('mysql:host=127.0.0.1;port=33060;dbname=sd', 'sd', $argv[1]);
+
+	echo "Getting documents\n";
+	$rows = $pdo->query('SELECT uid, path, keywords FROM files WHERE path LIKE "/home/bestuur/bestuur/standaarddocumenten/Minutes/%" AND path LIKE "%.pdf"');
+
+	echo "Magic!\n";
+	foreach ($rows as $row)
+	{
+		$date = null;
+
+		if (preg_match('/\/[^\/]*(20\d{2})-?(\d{1,2})-?(\d{1,2})[^\/]+$/', $row['path'], $match))
+			$date = new DateTime(sprintf('%04d-%02d-%02d', $match[1], $match[2], $match[3]));
+		
+		elseif (preg_match('/\/[^\/]*(\d{1,2})-(\d{1,2})-(20\d{2})[^\/]+$/', $row['path'], $match))
+			$date = new DateTime(sprintf('%04d-%02d-%02d', $match[3], $match[2], $match[1]));
+
+		else
+			continue;
+		
+		if (!preg_match_all('/^(Besluit|Hamerslag)(?!en)(.+?)(\s{2,})/ism', $row['keywords'], $matches))
+			continue;
+
+		foreach ($matches[2] as $i => $line)
+		{
+			$line = preg_replace("/\d+/", " ", $line);
+
+			$line = preg_replace("/\s+/", " ", $line);
+
+			$line = trim($line);
+
+			$commissie = null;
+
+			$actie = null;
+
+			$commissieleden = array();
+
+			$coverage = array();
+			
+			if (preg_match('/(uit|in)ge(-\s)?hamerd/i', $line, $match, PREG_OFFSET_CAPTURE)
+				|| preg_match('/(uit|in)\sde\s(.+?)\sge(-\s)?hamerd/', $line, $match, PREG_OFFSET_CAPTURE)
+				|| preg_match('/gaat\sde\s(.+?)\s(uit|in)/', $line, $match, PREG_OFFSET_CAPTURE)) {
+
+				$actie = $match[1][0] . 'gehamerd';
+				$coverage[] = array($match[0][1], strlen($match[0][0]));
+			}
+			
+			if (preg_match('/' . implode('|', array_keys($commissies)) . '/i', $line, $match, PREG_OFFSET_CAPTURE)) {
+				$commissie = $commissies[strtolower($match[0][0])];
+				$coverage[] = array($match[0][1], strlen($match[0][0]));
+			}
+			
+			foreach ($members as $id => $member) {
+				if ((int) $member['iter']->get('beginjaar') > (int) $date->format('Y'))
+					continue;
+
+				if (preg_match('/' . $member['pattern'] . '/is', $line, $match, PREG_OFFSET_CAPTURE)) {
+					$commissieleden[$id] = $member['iter'];
+					$coverage[] = array($match[0][1], strlen($match[0][0]));
+				}
+			}
+
+			if (!empty($commissieleden) && $actie && $commissie)
+				commit_membership($actie, $commissie, $commissieleden, $date);
+			
+			echo "\n" . format_coverage($line, $coverage) . "\n";
+			printf("%s %s %s %s: %s\033[0m\n",
+				!empty($commissieleden) && $actie && $commissie
+					? "\033[42m✓"
+					: "\033[41m✗",
+				$date->format('Y-m-d'),
+				$actie ? $actie : '[NULL]',
+				$commissie ? $commissie->get('naam') : '[NULL]',
+				implode(', ', array_map('member_full_name', $commissieleden)));
+		}
+	}
+}
+
+function commit_membership($actie, $commissie, $commissieleden, $date)
+{
+	global $membership_model;
+
+	if ($actie == 'ingehamerd') {
+		foreach ($commissieleden as $commissielid) {
+			$membership_model->start_membership($commissie, $commissielid, null, $date);
+		}
+	}
+	elseif ($actie == 'uitgehamerd') {
+		foreach ($commissieleden as $commissielid) {
+			$membership = $membership_model->find_membership($commissie, $commissielid, $date);
+
+			if (!$membership)
+				$membership = $membership_model->start_membership($commissie, $commissielid, null, null);
+			
+			$membership_model->end_membership($membership->get_id(), $date);
+		}
+	}
+}
+
+if ($argc > 1)
+	import_from_sd();
+else
+	import_from_file(fopen('php://stdin', 'r'));
