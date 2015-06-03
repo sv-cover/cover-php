e419e01f884dea0cb50ba4a6ef0e71dfb1888943
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-06-03 21:52:03 +0200

    Guess what is not surpressed by default :(

--- include/init.php
+++ include/init.php
@@ -8,9 +8,9 @@
 	ini_set('magic_quotes_gpc', 0);
 	
 	if (version_compare(PHP_VERSION, '5.4.0') < 0)
-		error_reporting(E_ALL ^ E_NOTICE);
+		error_reporting(E_ALL ^ E_NOTICE ^ E_USER_NOTICE);
 	else
-		error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
+		error_reporting(E_ALL ^ E_NOTICE ^ E_USER_NOTICE ^ E_DEPRECATED ^ E_STRICT);
 
 	require_once 'include/functions.php';
 	require_once 'include/i18n.php';
