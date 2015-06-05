1a443c3942cc02059ec17b757f4d84c090e703fb
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-06-05 17:13:43 +0200

    Quickly hack in support for GIF

--- fotoboek.php
+++ fotoboek.php
@@ -599,7 +599,11 @@
 			header('Pragma: public');
 			header('Cache-Control: max-age=86400');
 			header('Expires: '. gmdate('D, d M Y H:i:s \G\M\T', time() + 86400));
-			header('Content-Type: image/jpeg');
+			
+			if (substr($photo->get('filepath'), -3, 3) == 'gif')
+				header('Content-Type: image/gif');
+			else
+				header('Content-Type: image/jpeg');
 			
 			fpassthru($fhandle);
 			fclose($fhandle);
--- include/models/DataModelFotoboek.php
+++ include/models/DataModelFotoboek.php
@@ -137,32 +137,47 @@
 				$imagick = new Imagick();
 				$imagick->readImage($this->get_full_path());
 
-				// Rotate the image according to the EXIF data
-				switch($imagick->getImageOrientation())
+				if ($imagick->getImageFormat() == 'GIF')
 				{
-					case imagick::ORIENTATION_BOTTOMRIGHT:
-						$imagick->rotateimage('#000', 180); // rotate 180 degrees 
-						break; 
+					$gifmagick = $imagick->coalesceImages();
 
-					case imagick::ORIENTATION_RIGHTTOP:
-						$imagick->rotateimage('#000', 90); // rotate 90 degrees CW 
-						break; 
+					do {
+						$gifmagick->resizeImage($scaled_width, $scaled_height, Imagick::FILTER_BOX, 1);
+					} while ($gifmagick->nextImage());
 
-					case imagick::ORIENTATION_LEFTBOTTOM:
-						$imagick->rotateimage('#000', -90); // rotate 90 degrees CCW 
-						break;
+					$imagick = $gifmagick->deconstructImages();
+					$imagick->writeImagesFile($fhandle);
+				}
+				else
+				{
+					// Rotate the image according to the EXIF data
+					switch($imagick->getImageOrientation())
+					{
+						case imagick::ORIENTATION_BOTTOMRIGHT:
+							$imagick->rotateimage('#000', 180); // rotate 180 degrees 
+							break; 
+
+						case imagick::ORIENTATION_RIGHTTOP:
+							$imagick->rotateimage('#000', 90); // rotate 90 degrees CW 
+							break; 
+
+						case imagick::ORIENTATION_LEFTBOTTOM:
+							$imagick->rotateimage('#000', -90); // rotate 90 degrees CCW 
+							break;
+					}
+
+					// Scale the image
+					$imagick->scaleImage($scaled_width, $scaled_height);
+
+					// Strip EXIF data
+					$imagick->stripImage();
+
+					// Write the image as a progressive JPEG
+					$imagick->setImageFormat('jpg');
+					$imagick->setInterlaceScheme(Imagick::INTERLACE_PLANE);
+					$imagick->writeImageFile($fhandle);
 				}
 
-				// Scale the image
-				$imagick->scaleImage($scaled_width, $scaled_height);
-
-				// Strip EXIF data
-				$imagick->stripImage();
-
-				// Write the image as a progressive JPEG
-				$imagick->setImageFormat('jpg');
-				$imagick->setInterlaceScheme(Imagick::INTERLACE_PLANE);
-				$imagick->writeImageFile($fhandle);
 				$imagick->destroy();
 				fclose($fhandle);
 			}
