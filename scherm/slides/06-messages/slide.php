ee946491690c9183cb3f2b249b8d6497c4c87c0e
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-09-21 18:26:12 +0200

    Reverse

--- scherm/slides/06-messages/slide.php
+++ scherm/slides/06-messages/slide.php
@@ -3,7 +3,7 @@ $model = get_model('DataModelMessage');
 ?>
 <p style="font: 44px sans-serif;">Send in a message through https://www.svcover.nl/message.php</p>
 <ul>
-<?php foreach ($model->get_latest(10) as $message): ?>
-	<li style="font: 44px sans-serif;"><?=str_ireplace('cover', '<strong style="color:#c60c30">Cover</strong>', markup_format_text(substr($message->message, 0, 256))) ?></li>
+<?php foreach (array_reverse($model->get_latest(10)) as $message): ?>
+	<li style="font: 44px sans-serif;"><?=str_ireplace('cover', '<strong style="color:#c60c30">Cover</strong>', markup_format_text($message->message)) ?></li>
 <?php endforeach ?>
 </ul>
