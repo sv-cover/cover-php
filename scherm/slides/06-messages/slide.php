a1c3547934d1767afd1a1e1f1066c7324f7da4cc
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-09-21 17:48:41 +0200

    Larger.

--- scherm/slides/06-messages/slide.php
+++ scherm/slides/06-messages/slide.php
@@ -1,9 +1,9 @@
 <?php
 $model = get_model('DataModelMessage');
 ?>
-<p style="font: 22px sans-serif;">Send in a message through https://www.svcover.nl/message.php</p>
+<p style="font: 44px sans-serif;">Send in a message through https://www.svcover.nl/message.php</p>
 <ul>
 <?php foreach ($model->get_latest(10) as $message): ?>
-	<li style="font: 22px sans-serif;"><?=markup_format_text($message->message) ?></li>
+	<li style="font: 44px sans-serif;"><?=markup_format_text($message->message) ?></li>
 <?php endforeach ?>
 </ul>
