fcdc168074af0cbd7f509eabfed669b7872c73fd
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-09-21 17:39:05 +0200

    Fewer

--- scherm/slides/06-messages/slide.php
+++ scherm/slides/06-messages/slide.php
@@ -3,7 +3,7 @@ $model = get_model('DataModelMessage');
 ?>
 <p>Send in a message through https://www.svcover.nl/message.php</p>
 <ul>
-<?php foreach ($model->get_latest(30) as $message): ?>
+<?php foreach ($model->get_latest(10) as $message): ?>
 	<li><?=markup_format_text($message->message) ?></li>
 <?php endforeach ?>
 </ul>
