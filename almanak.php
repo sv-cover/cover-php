bc45a1e58a6a9db9cc5fc3d495210e602d0c919f
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-12-06 00:51:34 +0100

    Improve CSV and photo export for YearbookCee

--- almanak.php
+++ almanak.php
@@ -70,40 +70,90 @@
 			$this->get_content('almanak', $iters);
 		}
 		
-		protected function _process_csv()
+		protected function _process_export_csv()
 		{
-			if (member_in_commissie(COMMISSIE_ALMANAKCIE)) {
-				$iters = $this->model->get_from_search_first_last(null, null);
+			if (!get_identity()->member_in_committee(COMMISSIE_ALMANAKCIE))
+				throw new UnauthorizedException('Only members of the YearbookCee committee are allowed to download these dumps.');
 
-				// Filter all previous and hidden members
-				$this->model->visible_types = array(MEMBER_STATUS_LID,
-					MEMBER_STATUS_ERELID, MEMBER_STATUS_DONATEUR);
+			$iters = $this->model->get_from_search_first_last(null, null);
 
-				$iters = array_filter($iters, array($this->model, 'is_visible'));
+			// Filter all previous and hidden members
+			$this->model->visible_types = array(MEMBER_STATUS_LID,
+				MEMBER_STATUS_ERELID, MEMBER_STATUS_DONATEUR);
 
-				// Filter all hidden information (set the field to null)
-				$privacy_fields = $this->model->get_privacy();
+			$iters = array_filter($iters, array($this->model, 'is_visible'));
 
-				// Remove the fields that have to be exported
-				unset($privacy_fields['voornaam'], $privacy_fields['achternaam']);
+			// Filter all hidden information (set the field to null)
+			$privacy_fields = $this->model->get_privacy();
 
-				foreach ($iters as $iter)
-				{
-					foreach ($iter->data as $field => $value)
-						if (array_key_exists($field, $privacy_fields))
-							if (($this->model->get_privacy_for_field($iter, $field) & 1) === 0)
-								$iter->data[$field] = null;
+			// Remove the fields that have to be exported
+			unset($privacy_fields['voornaam'], $privacy_fields['achternaam']);
 
-					$iter->data['status'] = $this->model->get_status($iter);
+			foreach ($iters as $iter)
+			{
+				foreach ($iter->data as $field => $value)
+					if (array_key_exists($field, $privacy_fields))
+						if (($this->model->get_privacy_for_field($iter, $field) & 1) === 0)
+							$iter->data[$field] = null;
 
-					$iter->data['studie'] = implode(', ', $iter->get('studie'));
-				}
+				$iter->data['status'] = $this->model->get_status($iter);
 
-				run_view('almanak::csv',$this->model,$iters,null);
+				$iter->data['studie'] = implode(', ', $iter->get('studie'));
 			}
-			else {
-				$this->get_content('auth');
+
+			run_view('almanak::csv', $this->model, $iters, null);
+		}
+
+		protected function _process_export_photos()
+		{
+			if (!get_identity()->member_in_committee(COMMISSIE_ALMANAKCIE))
+				throw new UnauthorizedException('Only members of the YearbookCee committee are allowed to download these dumps.');
+
+			// Flush all of the current output and turn of the buffer
+			while (ob_get_level() > 0)
+				ob_end_clean();
+
+			// Disable PHP's time limit
+			set_time_limit(0);
+
+			// Make sure we stop when the user is no longer listening
+			ignore_user_abort(false);
+
+			// Set up the output zip stream and just handle all files as large files
+			// (meaning no compression, streaming stead of reading into memory.)
+			$zip = new ZipStream\ZipStream('almanac-' . date('Y-m-d') . '.zip', [
+				ZipStream\ZipStream::OPTION_LARGE_FILE_SIZE => 1,
+				ZipStream\ZipStream::OPTION_LARGE_FILE_METHOD => 'store',
+				ZipStream\ZipStream::OPTION_OUTPUT_STREAM => fopen('php://output', 'wb')]);
+
+			// Now for each book find all photos and add them to the zip stream
+			$iters = $this->model->get_from_search_first_last(null, null);
+
+			// Filter all previous and hidden members
+			$this->model->visible_types = [MEMBER_STATUS_LID, MEMBER_STATUS_ERELID, MEMBER_STATUS_DONATEUR];
+
+			$iters = array_filter($iters, [$this->model, 'is_visible']);
+
+			// Filter all hidden information (set the field to null)
+			$privacy_fields = $this->model->get_privacy();
+
+			foreach ($iters as $iter)
+			{
+				// Skip all members that have hidden their photo
+				if (($this->model->get_privacy_for_field($iter, 'foto') & 1) === 0)
+					continue;
+
+				// Skip members that don't have a photo
+				if (($data = $this->model->get_photo($iter)) === null)
+					continue;
+
+				$metadata = ['time' => $this->model->get_photo_mtime($iter)];
+
+				// And finally add the photo to the actual stream
+				$zip->addFile(sprintf('%d.jpg', $iter->get_id()), $data, $metadata);
 			}
+
+			$zip->finish();
 		}
 		
 		protected function run_impl() {
@@ -113,8 +163,10 @@
 				$this->_process_year();
 			elseif (isset($_GET['status']))
 				$this->_process_status();
-			elseif (isset($_GET['csv']))
-				$this->_process_csv();
+			elseif (isset($_GET['export']) && $_GET['export'] == 'csv')
+				$this->_process_export_csv();
+			elseif (isset($_GET['export']) && $_GET['export'] == 'photos')
+				$this->_process_export_photos();
 			else
 				$this->get_content('almanak');
 		}
