4152deafdc65422335f175136b5a327d9ca8b317
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2016-08-29 15:40:14 +0200

    Translate the settings controller to Twig.

--- include/policies/PolicyConfiguratie.php
+++ include/policies/PolicyConfiguratie.php
@@ -1,26 +1,24 @@
 <?php
 
-include 'include/member.php';
-
 class PolicyConfiguratie implements Policy
 {
 	public function user_can_create()
 	{
-		return member_in_commissie(COMMISSIE_EASY);
+		return get_identity()->member_in_committee(COMMISSIE_EASY);
 	}
 
 	public function user_can_read(DataIter $entry)
 	{
-		return member_in_commissie(COMMISSIE_EASY);
+		return get_identity()->member_in_committee(COMMISSIE_EASY);
 	}
 
 	public function user_can_update(DataIter $entry)
 	{
-		return member_in_commissie(COMMISSIE_EASY);
+		return get_identity()->member_in_committee(COMMISSIE_EASY);
 	}
 
 	public function user_can_delete(DataIter $entry)
 	{
-		return member_in_commissie(COMMISSIE_EASY);
+		return get_identity()->member_in_committee(COMMISSIE_EASY);
 	}
 }
--- themes/default/views/settings/settings.php
+++ themes/default/views/settings/settings.php
@@ -1,8 +1,6 @@
 <?php
-require_once 'include/markup.php';
-require_once 'include/form.php';
 
 class SettingsView extends CRUDView
 {
-	protected $__file = __FILE__;
+	//
 }
