9f293ee31c60063d9043eed2e21b69ece17a840e
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2016-09-04 11:04:46 +0200

    Disable photobook faces for non-logged-in people

--- include/policies/PolicyFotoboekFaces.php
+++ include/policies/PolicyFotoboekFaces.php
@@ -6,21 +6,21 @@ class PolicyFotoboekFaces implements Policy
 {
 	public function user_can_create()
 	{
-		return logged_in();
+		return get_auth()->logged_in();
 	}
 
 	public function user_can_read(DataIter $face)
 	{
-		return true;
+		return get_auth()->logged_in();
 	}
 
 	public function user_can_update(DataIter $face)
 	{
-		return logged_in();
+		return get_auth()->logged_in();
 	}
 
 	public function user_can_delete(DataIter $face)
 	{
-		return logged_in();
+		return get_auth()->logged_in();
 	}
 }
