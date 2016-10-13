b387c4e2fed160cd0a93f202f60f4989af0b70d3
martijnluinstra <martijnluinstra@gmail.com>
2016-10-13 15:08:53 +0200

    Reintroduce horizontal banner

--- themes/default/views/homepage/homepage.php
+++ themes/default/views/homepage/homepage.php
@@ -3,4 +3,13 @@
  
 	class HomepageView extends View {
 		protected $__file = __FILE__;
+
+		protected function render_banners(){
+			require_once dirname(__FILE__) . '/../Rotator.php';
+			$rotator = new Rotator('images/sponsors/');
+			$banners = $rotator -> get(15);
+
+			foreach ($banners as $banner)
+				echo '<a href="'.$banner['url'].'" target="_blank"><img src="images/sponsors/'.$banner['filename'].'" class="frontpage_banner"></a>';
+		}
 	}
