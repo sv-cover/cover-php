41a06673252cddd7d50421da6ede69e61276284a
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2016-10-31 16:15:50 +0100

    Move blog related view functions to view

--- themes/default/views/weblog/weblog.php
+++ themes/default/views/weblog/weblog.php
@@ -23,5 +23,32 @@ class WeblogView extends View
 			return 'images/heads/' . $iter['author'] . '.png';
 		else
 			return 'foto.php?lid_id=' . $iter['author'] . '&format=square&width=200';
-	}	
+	}
+
+	public function get_author_link(DataIter $message, $last = false)
+	{
+		if ($last && $message['last_author_type'])
+			$field = 'last_author';
+		else
+			$field = 'author';
+
+		try {
+			switch (intval($message[$field . '_type']))
+			{
+				case DataModelForum::TYPE_PERSON: /* Person */
+					return 'profiel.php?lid=' . $message[$field];
+				
+				case DataModelForum::TYPE_COMMITTEE: /* Commissie */
+					$committee_model = get_model('DataModelCommissie');
+					$committee = $committee_model->get_iter($message[$field]);
+					return 'commissies.php?commissie=' . urlencode($committee['login']);
+				
+				default:
+					return null;
+			}
+		} catch (DataIterNotFoundException $e) {
+			// Sometimes an author just doesnt exist anymore in the database. That's legacy for you!
+			return null;
+		}
+	}
 }
