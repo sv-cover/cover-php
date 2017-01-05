5a2720b24aa8639906e62839fdf4ff36e8987c97
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2017-01-05 21:43:27 +0100

    Document code a bit better.

--- include/models/DataModelCommitteeBattleScore.php
+++ include/models/DataModelCommitteeBattleScore.php
@@ -3,20 +3,6 @@ require_once 'include/data/DataModel.php';
 
 class DataIterCommitteeBattleScore extends DataIter
 {
-	public function get_committees()
-	{
-		return count($this['committee_ids'])
-			? get_model('DataModelCommissie')->find(['id__in' => $this['committee_ids']])
-			: [];
-	}
-
-	public function get_members()
-	{
-		return count($this['member_ids'])
-			? get_model('DataModelMember')->find(['id__in' => $this['member_ids']])
-			: [];
-	}
-
 	public function get_committee_ids()
 	{
 		return $this->model->db->query_column(
@@ -29,6 +15,20 @@ class DataIterCommitteeBattleScore extends DataIter
 		return $this->model->db->query_column(
 			sprintf("SELECT member_id FROM committee_battle_users WHERE score_id = %d",
 				$this->get_id()));
+	}
+	
+	public function get_committees()
+	{
+		return count($this['committee_ids'])
+			? get_model('DataModelCommissie')->find(['id__in' => $this['committee_ids']])
+			: [];
+	}
+
+	public function get_members()
+	{
+		return count($this['member_ids'])
+			? get_model('DataModelMember')->find(['id__in' => $this['member_ids']])
+			: [];
 	}
 }
 
@@ -48,10 +48,18 @@ class DataModelCommitteeBattleScore extends DataModel
 		parent::__construct($db, 'committee_battle_scores');
 	}
 
+	/**
+	 * Override of DataModel::_insert() to support setting the
+	 * committee_ids and member_ids properties. Also sets
+	 * the awarded_on field.
+	 */
 	protected function _insert($table, DataIter $iter, $get_id = false)
 	{
+		// Let PostgreSQL set the awarded_on field. 
 		$iter->set_literal('awarded_on', 'current_timestamp');
 
+		// _insert() will behave thanks to $this->fields not
+		// including 'committee_ids' and 'member_ids'.
 		$id = parent::_insert($table, $iter, true);
 
 		if (isset($iter->data['committee_ids']))
@@ -60,13 +68,21 @@ class DataModelCommitteeBattleScore extends DataModel
 		if (isset($iter->data['member_ids']))
 			$this->_set_members($iter->data['member_ids'], $id);
 
+		// Recreate the behaviour of DataModel::_insert()'s third argument
 		return $get_id ? $id : -1;
 	}
 
+	/**
+	 * Override of DataModel::_update() to support setting the
+	 * committee_ids and member_ids properties.
+	 */
 	protected function _update($table, DataIter $iter)
 	{
+		// _update() won't create a weird query because it will only update 
+		// the fields defined in $this->fields.
 		$affected_rows = parent::_update($table, $iter);
 
+		// Call these helper methods if the data is present in the score object.
 		if (isset($iter->data['committee_ids']))
 			$this->_set_committees($iter->data['committee_ids'], $iter->get_id());
 
@@ -76,6 +92,11 @@ class DataModelCommitteeBattleScore extends DataModel
 		return $affected_rows;
 	}
 
+	/**
+	 * Helper method to link committees who scored the points to a certain score object.
+	 * @param int[] $committee_ids
+	 * @param int $score_id
+	 */
 	protected function _set_committees($committee_ids, $score_id)
 	{
 		$pairs = [];
@@ -90,6 +111,11 @@ class DataModelCommitteeBattleScore extends DataModel
 			implode(', ', $pairs)));
 	}
 
+	/**
+	 * Helper method to link members who helped obtain the points to a certain score object.
+	 * @param int[] $member_ids
+	 * @param int $score_id
+	 */
 	protected function _set_members($member_ids, $score_id)
 	{
 		$pairs = [];
@@ -104,6 +130,11 @@ class DataModelCommitteeBattleScore extends DataModel
 			implode(', ', $pairs)));
 	}
 
+	/**
+	 * Override DataIter::_generate_conditions_from_array() to support
+	 * filtering on committee_id and member_id, which is done through
+	 * subqueries because this data lives in different tables.
+	 */
 	protected function _generate_conditions_from_array(array $conditions)
 	{
 		$fallback = [];
@@ -132,16 +163,29 @@ class DataModelCommitteeBattleScore extends DataModel
 		return implode(' AND ', $atoms);
 	}
 
+	/**
+	 * Override DataModel::_generate_query() to change default sort order.
+	 */
 	protected function _generate_query($where)
 	{
 		return parent::_generate_query($where) . ' ORDER BY awarded_on DESC';
 	}
 
+	/**
+	 * Get all scores for a single committee.
+	 * @param DataIterCommissie $committee
+	 * @return DataIterCommitteeBattleScore[]
+	 */
 	public function get_for_committee(DataIterCommissie $committee)
 	{
 		return $this->find(['committee_id' => $committee['id']]);
 	}
 
+	/**
+	 * Get score totals for a list of committees.
+	 * @param DataIterCommissie[] $committee
+	 * @return int[] associated array with commitee id as key and score as value.
+	 */ 
 	public function get_scores_for_committees($committees)
 	{
 		$query = "
