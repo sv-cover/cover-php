ab3aada39a5c938072809e2ade728be1a68b140c
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2017-01-05 22:12:52 +0100

    Saving arrays is a bit difficult this way.

--- include/controllers/ControllerCRUD.php
+++ include/controllers/ControllerCRUD.php
@@ -30,10 +30,7 @@ class ControllerCRUD extends Controller
 	protected function _update(DataIter $iter, $data, array &$errors)
 	{
 		foreach ($data as $key => $value)
-			if (is_scalar($value))
-				$iter->set($key, trim($value));
-			elseif (is_null($value))
-				$iter->set($key, null);
+			$iter->set($key, $value);
 
 		return $this->model->update($iter) > 0;
 	}
