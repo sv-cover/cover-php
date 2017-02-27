5109452fa4308893176270138547746ef3ba6ba0
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2017-02-27 23:54:09 +0100

    Forgot to add password_reset_tokens table to structure.sql

--- include/data/structure.sql
+++ include/data/structure.sql
@@ -594,3 +594,13 @@ CREATE TABLE applications (
     name TEXT NOT NULL,
     secret TEXT NOT NULL
 );
+
+--
+-- Temporarily stored password (re)set codes.
+--
+
+CREATE TABLE password_reset_tokens (
+    key character (40) PRIMARY KEY,
+    member_id integer NOT NULL REFERENCES leden (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    created_on timestamp without time zone NOT NULL
+);
