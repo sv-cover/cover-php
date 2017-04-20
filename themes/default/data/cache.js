5223ccae2c5954870aa1592ad2fdd1fbdef98885
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2017-04-20 10:23:08 +0200

    Move the style from the 'open' label to the stylesheet.

--- themes/default/data/cache.js
+++ themes/default/data/cache.js
@@ -7,21 +7,7 @@ jQuery(function($) {
 
 		var label = $('<span>')
 			.addClass('cover-room-' + status)
-			.text(status)
-			.css({
-				'position': 'absolute',
-				'top': '35px',
-				'left': '260px',
-				'font-size': '1.7em',
-				'text-transform': 'uppercase',
-				'display': 'inline-block',
-				'background': '#C8102E',
-				'color': 'white',
-				'padding': '4px 18px',
-				'border-radius': '4px',
-				'box-shadow': '0 0 0 2px white, 0 0 0 4px #C8102E, 0 0 4px 4px rgba(0, 0, 0, 0.6)',
-				'transform': 'rotate(-7deg)'
-			});
+			.text(status);
 		$('.world > .header').append(label);
 	});
 });
