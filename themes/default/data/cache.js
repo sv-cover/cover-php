215c3ed65aeaced9295c626dc2295de4c72e6132
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2017-04-24 20:37:10 +0200

    Make the 'the cover room is open' a lot clearer

--- themes/default/data/cache.js
+++ themes/default/data/cache.js
@@ -1,13 +1,15 @@
 jQuery(function($) {
 	$.ajax('https://cache.svcover.nl/').then(function(response) {
-			var status = response.match(/<p>.*\bopen\b.*<\/p>/i) ? 'open' : 'closed';
+		var status = response.match(/<p>.*\bopen\b.*<\/p>/i) ? 'open' : 'closed';
 
 		if (status != 'open')
 			return;
 
-		var label = $('<span>')
+		var label = $('<a>')
+			.attr('href', 'https://cache.svcover.nl/')
 			.addClass('cover-room-' + status)
-			.text(status);
+			.text('the Cover room is')
+			.append($('<span>').text(status));
 		$('.world > .header').append(label);
 	});
 });
