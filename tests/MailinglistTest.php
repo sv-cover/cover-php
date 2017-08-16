85e54258517c70ed79920984973ed8b456511044
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2017-08-16 16:27:57 +0200

    Show what is wrong if the mailing test case fails

--- tests/MailinglistTest.php
+++ tests/MailinglistTest.php
@@ -14,6 +14,9 @@ class MailinglistTest extends TestCase
 	{
 		$result = $this->simulateEmail('testcase@example.com', 'does-not-exist@svcover.nl', 'Hello world!', ['Subject: test-mail']);
 
+		if ($result->exit_code == 255)
+			$result->write();
+
 		$this->assertEquals(103, $result->exit_code, 'Expect the script to return the error code RETURN_COULD_NOT_DETERMINE_LIST');
 
 		$this->assertCount(1, $result->messages, 'Expect it also to send a return message to the sender');
