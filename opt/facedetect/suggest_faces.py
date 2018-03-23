dbbf496f3915784a3148da9fa06460f02cefd6d5
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2018-03-23 22:39:43 +0100

    Update face suggest to use dlib instead of OpenCV

--- opt/facedetect/suggest_faces.py
+++ opt/facedetect/suggest_faces.py
@@ -1,43 +1,32 @@
-import numpy as np
-import cv2
+from __future__ import print_function, division
+
 import psycopg2
-import urllib2
+from PIL import Image
+import numpy as np
+import dlib
 import sys
 import os
 
-opencv_shared = os.path.dirname(os.path.abspath(__file__)) + '/cascades/'
-face_cascade = cv2.CascadeClassifier(opencv_shared + 'haarcascade_frontalface_default.xml')
-
 conn = psycopg2.connect("dbname=webcie")
 cur = conn.cursor()
-
 icur = conn.cursor()
 
-def read_image_remote(url):
-	if url[0:2] == '//':
-		url = 'https:' + url
-	
-	request = urllib2.urlopen(url)
-	data = np.asarray(bytearray(request.read()), dtype=np.uint8)
-	image = cv2.imdecode(data, cv2.CV_LOAD_IMAGE_GRAYSCALE)
-
-	if image is None:
-		raise Exception("cv.imdecode could not decode image")
-
-	return image
+detector = dlib.get_frontal_face_detector()
 
 def read_image(path):
-	image = cv2.imread(path, cv2.CV_LOAD_IMAGE_GRAYSCALE)
-	if image is None:
-		raise Exception("cv.imdecode could not decode image")
-	return image
+	img = Image.open(path)
+	img.thumbnail((800,800)) # downscale it a bit for performance
+	return np.array(img), img.width, img.height
 
 def find_faces(path):
 	try:
-		gray = read_image(path)
-		ih, iw = gray.shape
-		faces = face_cascade.detectMultiScale(gray, 1.1, 4, 0, (int(iw * 0.05), int(ih * 0.05)))
-		return [(float(x / float(iw)), float(y / float(ih)), float(w / float(iw)), float(h / float(ih))) for (x, y, w, h) in faces]
+		img, width, height = read_image(path)
+		for face in detector(img, 1):
+			x = face.left() / width
+			y = face.top() / height
+			w = (face.right() - face.left()) / width
+			h = (face.bottom() - face.top()) / height
+			yield (x, y, w, h)
 	except Exception as ex:
 		print("Skipping %s: %s" % (path, ex))
 		return []
@@ -72,17 +61,14 @@ if __name__ == '__main__':
 	cur.execute("SELECT id, filepath FROM fotos WHERE id = ANY (%s)", (photo_ids,));
 
 	for row in cur.fetchall():
-		faces = find_faces(os.path.join(sys.argv[1], row[1]))
-
 		print("%d:" % row[0])
-
-		for face in faces:
-			print("  x: %0.2f y: %0.2f w: %0.2f h: %0.2f" % face)
+		for face in find_faces(os.path.join(sys.argv[1], row[1])):
+			print("  x: %0.2f y: %0.2f w: %0.2f h: %0.2f" % face, end='')
 			if insert_face(row[0], face):
 				print("  added")
-				conn.commit()
 			else:
 				print("  duplicate")
+		conn.commit()
 
 	print("Finished.")
 	exit(0)
