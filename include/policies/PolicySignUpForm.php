2ce5c2698b6c8dee65638bdab02ef56f7f949e06
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2018-06-10 16:00:18 +0200

    Set up pilot for ActiviTee

--- include/policies/PolicySignUpForm.php
+++ include/policies/PolicySignUpForm.php
@@ -6,6 +6,14 @@ class PolicySignUpForm implements Policy
 {
 	public function user_can_create(DataIter $form)
 	{
+		// BEGIN Trial period, only for activitee right now
+		if (!get_identity()->member_in_committee(2)) // Activitee
+			return false;
+
+		if ($form['committee_id'] !== null && $form['committee_id'] != 2)
+			return false;
+		// END
+
 		if ($form['committee_id'] !== null)
 			return get_identity()->member_in_committee($form['committee_id']);
 		else
