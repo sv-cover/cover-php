eec88d30b8fff6093db82f3982ad97b2891006fc
martijnluinstra <martijnluinstra@gmail.com>
2018-06-20 16:06:47 +0200

    Remove notes from registration form, as the notes are actually conditions.

--- themes/default/views/lidworden/lidworden.php
+++ themes/default/views/lidworden/lidworden.php
@@ -8,11 +8,9 @@ class LidwordenView extends View
 
 		$voorwaarden = $editable_model->get_iter_from_title('Voorwaarden aanmelden');
 
-		$opmerkingen = $editable_model->get_iter_from_title('Opmerkingen aanmelden');
-
 		$academic_year = time() < mktime(0, 0, 0, 7, 1, date('Y')) ? date('Y') - 1 : date('Y');
 
-		return $this->render('lidworden.twig', compact('errors', 'voorwaarden', 'opmerkingen', 'academic_year'));
+		return $this->render('lidworden.twig', compact('errors', 'voorwaarden', 'academic_year'));
 	}
 
 	public function render_submitted()
