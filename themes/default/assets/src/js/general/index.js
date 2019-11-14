d73f3f634154245564ba6da85df10fe030991544
Martijn Luinstra <martijnluinstra@gmail.com>
2019-11-14 23:48:36 +0100

    Implement preview loader

--- themes/default/assets/src/js/general/index.js
+++ themes/default/assets/src/js/general/index.js
@@ -1,1 +1,2 @@
 import './darkmode';
+import './preview_loader';
---
+++ themes/default/assets/src/js/general/preview_loader.js
@@ -0,0 +1,63 @@
+import Bulma from '@vizuaalog/bulmajs/src/core';
+
+
+class PreviewLoader {
+    /**
+     * Get the root class this plugin is responsible for.
+     * This will tell the core to match this plugin to an element with a .modal class.
+     * @returns {string} The class this plugin is responsible for.
+     */
+    static getRootClass() {
+        return 'content-preview';
+    }
+
+
+    /**
+     * Handle parsing the DOMs data attribute API.
+     * @param {HTMLElement} element The root element for this instance
+     * @return {undefined}
+     */
+    static parse(element) {
+        new PreviewLoader({
+            element: element,
+            source: element.dataset.previewSource,
+            url: element.dataset.previewUrl
+        });
+    }
+
+    constructor(options) {
+        this.element = options.element;
+        this.url = options.url;
+        this.source = document.querySelector(options.source);
+        this.form = options.element.closest('form');
+
+        if (!this.source)
+            throw new Error('Preview source not found for "' + options.source + '"');
+
+        this.element.addEventListener('show-tab', this.loadPreview.bind(this));
+    }
+
+    async fetchPreview() {
+        const response = await fetch(this.url, {
+            method: 'POST',
+            body: new FormData(this.form)
+        });
+     
+        if (!response.ok) {
+            throw new Error(await response.text());
+        }
+
+        return await response.text();
+    }
+
+    loadPreview() {
+        this.fetchPreview()
+            .then(content => this.element.innerHTML = content)
+            .catch(error => console.warn(error));
+    }
+}
+
+
+Bulma.registerPlugin('preview-loader', PreviewLoader);
+
+export default PreviewLoader;
