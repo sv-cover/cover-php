f3272bde87317a5ecd70caa487dbc77d668f159e
Martijn Luinstra <martijnluinstra@gmail.com>
2020-01-10 18:18:29 +0100

    PhotoCee can sort photos and books

--- themes/default/assets/src/js/pages/photos/gallery_admin.js
+++ themes/default/assets/src/js/pages/photos/gallery_admin.js
@@ -1,8 +1,9 @@
 import {Bulma, AutoPopup} from 'cover-style-system/src/js';
+import Sortable from 'sortablejs';
 
 class PhotoGalleryAdmin {
     static parseDocument(context) {
-        let elements = context.querySelectorAll('.photo-gallery[data-permissions=admin]');
+        const elements = context.querySelectorAll('.photo-gallery[data-permissions=admin]');
 
         Bulma.each(elements, element => {
             new PhotoGalleryAdmin({
@@ -21,9 +22,12 @@ class PhotoGalleryAdmin {
         this.element = options.element;
         this.bookId = options.bookId;
         this.selectedIds = [];
+        this.sortableLists = [];
+        this.sortableActive = false;
 
         this.initCheckboxes();
-        this.deleteButton = this.initDeleteButton();
+        this.initDeleteButton();
+        this.initSortable();
 
         this.element.querySelectorAll('.photo-selection-control').forEach( el => {
             el.disabled = true;
@@ -46,24 +50,42 @@ class PhotoGalleryAdmin {
     }
 
     initDeleteButton() {
-        let buttons = this.element.querySelectorAll('#delete-selected-photos-button');
+        const button = this.element.querySelector('#delete-selected-photos-button');
 
-        Bulma.each(buttons, button => {
-            button.addEventListener('click', evt => {
-                if (this.selectedIds.length === 0)
-                    return;
+        button.addEventListener('click', this.handleDelete.bind(this));
+    }
 
-                const params = new URLSearchParams({
-                    book: this.bookId,
-                    view: 'delete_photos'
-                });
+    initSortable() {
+        this.sortableButton = this.element.querySelector('#order-photos-button');
+        this.sortableButton.addEventListener('click', this.handleSortable.bind(this));
+        this.photoOrderUrl = this.sortableButton.dataset.photoOrderUrl;
+        this.bookOrderUrl = this.sortableButton.dataset.bookOrderUrl;
 
-                for (const id of this.selectedIds)
-                    params.append('photo_id[]', id);
+        // Init sortable photo galleries
+        const photo_galleries = this.element.querySelectorAll('.gallery');
+        Bulma.each(photo_galleries, element => {
+            this.sortableLists.push(new Sortable(element, {
+                disabled: 'true',
+                handle: '.photo a',
+                onUpdate: this.handleSort.bind(this, this.photoOrderUrl)
+            }));
+        });
 
-                let request = fetch('fotoboek.php?' + params.toString());
-                new AutoPopup({contentType: 'modal'}, request);
-            });
+        // Init sortable book galleries
+        const book_galleries = this.element.querySelectorAll('.book-gallery');
+        Bulma.each(book_galleries, element => {
+            this.sortableLists.push(new Sortable(element, {
+                disabled: 'true',
+                handle: '.book',
+                onUpdate: this.handleSort.bind(this, this.bookOrderUrl)
+            }));
+        });
+
+        // Randomise wiggle
+        this.element.querySelectorAll('.gallery > .photo, .book-gallery > .column').forEach( element => {
+            const duration = getComputedStyle(element).getPropertyValue('--wiggle-duration');
+            const delay = -1 * parseFloat(duration) * Math.random();
+            element.style.setProperty("--wiggle-delay", delay + 's');
         });
     }
 
@@ -76,6 +98,52 @@ class PhotoGalleryAdmin {
 
         this.element.querySelectorAll('.photo-selection-control').forEach( el => {
             el.disabled = this.selectedIds.length === 0;
+        });
+    }
+
+    handleDelete(event) {
+        if (this.selectedIds.length === 0)
+            return;
+
+        const params = new URLSearchParams({
+            book: this.bookId,
+            view: 'delete_photos'
+        });
+
+        for (let id of this.selectedIds)
+            params.append('photo_id[]', id);
+
+        const request = fetch('fotoboek.php?' + params.toString());
+        new AutoPopup({contentType: 'modal'}, request);
+    }
+
+    handleSortable(event) {
+        if (this.sortableActive) {
+            this.sortableActive = false;
+            for (let list of this.sortableLists) {
+                list.option("disabled", true);
+                list.el.classList.remove('is-sortable');
+            }
+            this.sortableButton.classList.remove('is-active');
+        } else {
+            this.sortableActive = true;
+            for (let list of this.sortableLists){
+                list.option("disabled", false);
+                list.el.classList.add('is-sortable');
+            }
+            this.sortableButton.classList.add('is-active');
+        }
+    }
+
+    handleSort(url, event) {
+        const list = Sortable.get(event.to);
+        const data = new FormData();
+        for (let id of list.toArray())
+            data.append('order[]', id);
+
+        fetch(url, {
+            method: 'POST',
+            body: new URLSearchParams(data)
         });
     }
 }
