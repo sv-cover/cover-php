b8968fdd69d7d7d9ccc32802a841cec6d1a77b27
Vlad Iftime <vladiftime60@gmail.com>
2020-02-18 18:13:52 +0100

    Carrousel installed but not working

--- themes/default/assets/src/js/general/index.js
+++ themes/default/assets/src/js/general/index.js
@@ -1,1 +1,8 @@
 import './darkmode';
+import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js';
+
+// Initialize all elements with carousel class.
+bulmaCarousel.attach('#carousel-demo', {
+    slidesToScroll: 1,
+    slidesToShow: 4
+});
