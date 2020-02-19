f82391f8bda0e052083cad6d3a32bd3917489765
Vlad <vladiftime60@gmail.com>
2020-02-19 01:51:55 +0100

    some adjustments

--- themes/default/assets/src/js/index.js
+++ themes/default/assets/src/js/index.js
@@ -1,8 +1,2 @@
 import Bulma from 'cover-style-system/src/js';
-import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js';
 import './general';
-// Initialize all elements with carousel class.
-bulmaCarousel.attach('#carousel-demo', {
-    slidesToScroll: 1,
-    slidesToShow: 4
-});
