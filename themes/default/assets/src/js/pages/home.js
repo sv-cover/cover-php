82384771e14db39faec01179310825f99d53b8c1
Vlad <deea_toca@yahoo.com>
2020-03-04 14:36:30 +0100

    test

---
+++ themes/default/assets/src/js/pages/home.js
@@ -0,0 +1,8 @@
+
+import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js';
+
+// Initialize all elements with carousel class.
+bulmaCarousel.attach('#carousel-demo', {
+    slidesToScroll: 1,
+    slidesToShow: 4
+});
