0d73eba51093f1d58a51dc65ca4d3f3fc3bd7244
Vlad <deea_toca@yahoo.com>
2020-04-03 18:28:18 +0200

    become a member banner

--- themes/default/assets/src/js/pages/home.js
+++ themes/default/assets/src/js/pages/home.js
@@ -6,17 +6,35 @@ bulmaCarousel.attach('#carousel-demo', {
     slidesToShow: 2
 });
 
-document.getElementById("main").addEventListener("click", openNav);
-document.getElementById("mySidebar").addEventListener("click", closeNav);
+document.getElementById("spani").addEventListener("click", openNav);
+// document.getElementById("mySidebar").addEventListener("click", closeNav);
 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "500px";
-    document.getElementById("main").style.marginLeft = "500px";
+    console.log(document.getElementById("mySidebar").style.width);
+    if(document.getElementById("mySidebar").style.width == "500px") {
+      document.getElementById("mySidebar").style.width = "0px";
+      document.getElementById("main").style.marginLeft = "0px";
+    } else {
+      document.getElementById("mySidebar").style.width = "500px";
+      document.getElementById("main").style.marginLeft= "500px";  
+    }
+
+    
   }
   
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+  // function closeNav() {
+  //   document.getElementById("mySidebar").style.width = "0";
+  //   document.getElementById("main").style.marginLeft= "0";
+  // }
+
+document.getElementById("spani").addEventListener("click", animation);
+
+function animation() {
+    document.getElementById("spani").classList.toggle("left");
+    document.getElementById("spani").classList.toggle("right");
+}
 
 
+// $('.arrow--l-r').on('click', function() {
+//     $(this).toggleClass('left right');
+// });
