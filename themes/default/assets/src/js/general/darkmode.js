7ef8b0218f959dc3f7ce8924d9f2e08a2979790b
Martijn Luinstra <martijnluinstra@gmail.com>
2020-04-09 14:34:53 +0200

    Improve user menu

--- themes/default/assets/src/js/general/darkmode.js
+++ themes/default/assets/src/js/general/darkmode.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.dark-mode-switch').forEach(function(switchItem) {
+document.querySelectorAll('.dark-mode-switch .switch').forEach(function(switchItem) {
     switchItem.addEventListener('change', function(evt) {
         var state = this.checked ? 'dark' : 'light';
         document.cookie = 'cover_color_mode=' + state + '; expires=Fri, 31 Dec 9999 23:59:59 GMT";';
