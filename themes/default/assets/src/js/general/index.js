e8f76f948b3322337e42c58849e6ce74c15726fc
Martijn Luinstra <martijnluinstra@gmail.com>
2020-04-24 18:40:19 +0200

    Create generic Autocomplete plugin, that supports json and url based sources.

--- themes/default/assets/src/js/general/index.js
+++ themes/default/assets/src/js/general/index.js
@@ -1,6 +1,6 @@
 import './darkmode';
 import './lazy_image_loader';
-import './autocomplete_data';
+import './autocomplete';
 import './autocomplete_member';
 import './partners';
 import './preview_loader';
