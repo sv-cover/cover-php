e7a5cf54f1a959677f6e03da410508f8574d390d
Martijn Luinstra <martijnluinstra@gmail.com>
2020-06-07 00:43:41 +0200

    Use inline-action for add field, enable header button for delete

--- themes/default/assets/src/js/pages/signup/signup_form_form.js
+++ themes/default/assets/src/js/pages/signup/signup_form_form.js
@@ -15,34 +15,10 @@ class SignupFormForm {
 
     constructor(options) {
         this.element = options.element;
-        this.fieldListElement = options.element.querySelector('.signup-form-field-list');
-        this.addFieldForm = options.element.querySelector('.signup-form-field-form');
-        this.setupEvents();
-    }
 
-    setupEvents() {
-        this.addFieldForm.addEventListener('submit', this.handleAddField.bind(this));
-    }
-
-    async handleAddField(event) {
-        event.preventDefault();
-
-        const url = this.addFieldForm.dataset.asyncAction;
-        const data = new FormData(this.addFieldForm);
-        const init = {
-            method: 'POST',
-            body: new URLSearchParams(data),
-        };
-
-        const response = await fetch(url, init);
-        const text = await response.text();
-
-        const doc = (new DOMParser()).parseFromString(text, 'text/html');
-        const field = doc.querySelector('.signup-form-field');
-
-        this.fieldListElement.append(field);
-
-        document.dispatchEvent(new CustomEvent('partial-content-loaded', { bubbles: true, detail: field }));
+        const deleteButtons = this.element.querySelectorAll('.card-header .signup-form-field-delete-button');
+        for (let element of deleteButtons)
+            element.hidden = false;
     }
 }
 
