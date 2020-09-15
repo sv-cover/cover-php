a0397d55b391e36fb97089f521dccd84fa810347
Martijn Luinstra <martijnluinstra@gmail.com>
2020-09-16 01:34:53 +0200

    Fix sentry php 7.4 issues?

--- include/sentry.php
+++ include/sentry.php
@@ -4,50 +4,43 @@ function sentry_get_client()
 {
 	static $client = null;
 
-	if ($client === null)
-		$client = get_config_value('sentry_url')
-			? new Raven_Client(get_config_value('sentry_url'))
-			: false;
+	if ($client === null) {
+		if (get_config_value('sentry_url')) {
+			Sentry\init(['dsn' => get_config_value('sentry_url')]);
+			$client = true;
+		} else {
+			$client = false;
+		}
+	}
 
 	return $client ? $client : null;
 }
 
 function sentry_report_exception($e, $attributes = [])
 {
-	$client = sentry_get_client();
-
-	if ($client === null)
-		return null;
-
-	return $client->captureException($e, $attributes);
+	if (sentry_get_client())
+		return Sentry\captureException($e, $attributes);
+	return null;
 }
 
 function init_sentry()
 {
-	$client = sentry_get_client();
-
-	if (!$client)
+	if (!sentry_get_client())
 		return;
 
 	$client->tags_context([
 		'locale' => i18n_get_locale()
 	]);
 
-	if (get_auth()->logged_in()) {
-		$client->user_context([
-			'id' => get_identity()->get('id'),
-			'email' => get_identity()->get('email')
-		]);
+	Sentry\configureScope(function (Sentry\State\Scope $scope): void {
+		$scope->setTag('locale', 'i18n_get_locale()');
+		if (get_auth()->logged_in()) {
+			$scope->setUser([
+				'id' => get_identity()->get('id'),
+				'email' => get_identity()->get('email')
+			]);
 
-		$client->extra_context([
-			'session_id' => get_auth()->get_session()->get('id')
-		]);
-	}
-
-	// We already take care of the exceptions ourselves, and use our
-	// own error handler to turn warnings etc. into exceptions.
-	// However, some errors cannot be caught that way, so let's fall
-	// back on Sentry's client for those.
-	$error_handler = new Raven_ErrorHandler($client);
-	$error_handler->registerShutdownFunction();
+			$scope->setExtra('session_id', get_auth()->get_session()->get('id'));
+		}
+	});
 }
