0d2af6d7ed4c72499cc32f84d7424f94140f60a9
Martijn Luinstra <martijnluinstra@gmail.com>
2020-12-06 02:35:40 +0100

    Scope homepage js (did nobody check the photo pages yet?)

--- themes/default/assets/src/js/main/pages/home.js
+++ themes/default/assets/src/js/main/pages/home.js
@@ -13,74 +13,83 @@ function containedScrollIntoView(element) {
   });
 }
 
-// Initialize all elements with carousel class.
-var carousels = bulmaCarousel.attach('.carousel', {
-    slidesToScroll: 1,
-    slidesToShow: 1,
-    autoplay: true,
-    loop: true,
-    autoplaySpeed: 5000,
-    infinite: true,
-    breakpoints: [{
-      changePoint: 480,
+function init_homepage(element) {
+  // Initialize all elements with carousel class.
+  var carousels = bulmaCarousel.attach('.carousel', {
+      slidesToScroll: 1,
       slidesToShow: 1,
-      slidesToScroll: 1
-    },
-    {
-      changePoint: 640,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    },
-    {
-      changePoint: 768,
-      slidesToShow: 1,
-      slidesToScroll: 1
+      autoplay: true,
+      loop: true,
+      autoplaySpeed: 5000,
+      infinite: true,
+      breakpoints: [{
+        changePoint: 480,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      },
+      {
+        changePoint: 640,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      },
+      {
+        changePoint: 768,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    ],
+  });
+
+  // Activate the event in the calendar that corresponds with the one in the carousel
+
+  for(let i = 0; i < carousels.length; i++) {
+    //  The carousel event listener only activates after the first slide
+    // so we have to set up the first event manually
+    document.getElementById('event-0').classList.add('is-active')
+    
+  	carousels[i].on('before:show', state => {
+      // deactivate the rest of the events
+      for (let i = 0; i < state.length; ++i)
+        document.getElementById('event-' + i).classList.remove('is-active')
+
+      // the first slide is actually the second one so we need some math to get the order right
+      let event = 'event-' + ((state.index + 1) % state.length)
+      let element = document.getElementById(event);
+      element.classList.add('is-active');
+      containedScrollIntoView(element.closest('.event'));
+  	});
+  }
+
+  // Resize carousels with window
+  // TODO: This is still really buggy...
+  window.addEventListener('resize', () => {
+    for (let carousel of carousels) {
+      carousel.reset();
     }
-  ],
-});
+  });
 
-// Activate the event in the calendar that corresponds with the one in the carousel
+  // Get the announcements that are too long and make them collapsible
+  const half = document.getElementsByClassName("is-half-height")
 
-for(let i = 0; i < carousels.length; i++) {
-  //  The carousel event listener only activates after the first slide
-  // so we have to set up the first event manually
-  document.getElementById('event-0').classList.add('is-active')
-  
-	carousels[i].on('before:show', state => {
-    // deactivate the rest of the events
-    for (let i = 0; i < state.length; ++i)
-      document.getElementById('event-' + i).classList.remove('is-active')
+  for (let element of half) {
+    let h = element.getElementsByClassName("card-content")
+    if (h[1] && h[1].clientHeight > 400){
+        element.getElementsByClassName("controls")[0].classList.remove("is-not-active-read-more")
+        element.getElementsByClassName("controls")[0].classList.add("is-active-read-more")
+        h[1].classList.add("is-half-height")
+        h[1].classList.add("is-long-text")
+        h[1].classList.add("collapse-content");
 
-    // the first slide is actually the second one so we need some math to get the order right
-    let event = 'event-' + ((state.index + 1) % state.length)
-    let element = document.getElementById(event);
-    element.classList.add('is-active');
-    containedScrollIntoView(element.closest('.event'));
-	});
+        let collapse_element = element.closest('.card');
+        collapse_element.classList.add('collapse');
+        new Collapse({element: collapse_element});
+    }
+  }
 }
 
-// Resize carousels with window
-// TODO: This is still really buggy...
-window.addEventListener('resize', () => {
-  for (let carousel of carousels) {
-    carousel.reset();
-  }
-});
 
-// Get the announcements that are too long and make them collapsible
-const half = document.getElementsByClassName("is-half-height")
+const homepage = document.querySelector('.homepage');
 
-for (let element of half) {
-  let h = element.getElementsByClassName("card-content")
-  if (h[1] && h[1].clientHeight > 400){
-      element.getElementsByClassName("controls")[0].classList.remove("is-not-active-read-more")
-      element.getElementsByClassName("controls")[0].classList.add("is-active-read-more")
-      h[1].classList.add("is-half-height")
-      h[1].classList.add("is-long-text")
-      h[1].classList.add("collapse-content");
-
-      let collapse_element = element.closest('.card');
-      collapse_element.classList.add('collapse');
-      new Collapse({element: collapse_element});
-  }
+if (homepage) {
+  init_homepage(homepage);
 }
