dfae46d1cdd67b329bc1e2b99de8197446749344
Martijn Luinstra <martijnluinstra@gmail.com>
2022-09-24 13:24:53 +0200

    Filemanager field

---
+++ src/Form/Type/FilemanagerType.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Form\Type;
+
+use App\Validator\FilemanagerFile;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
+class FilemanagerType extends AbstractType
+{
+    public function getParent(): string
+    {
+        return TextType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'constraints' => [
+                new Length(['max' => 255]),
+                new FilemanagerFile(),
+            ]
+        ]);
+    }
+}
