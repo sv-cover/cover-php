a63c108557acc09e4a89d10420547d822a51557c
Martijn Luinstra <martijnluinstra@gmail.com>
2023-01-28 03:00:14 +0100

    Use page cover photo for former board pictures

--- public/views/boards/boards.php
+++ public/views/boards/boards.php
@@ -2,25 +2,5 @@
 
 class BoardsView extends CRUDView
 {
-	public function get_summary($page_id)
-	{
-		$editable_model = get_model('DataModelEditable');
-		
-		return $editable_model->get_summary($page_id);
-	}
-
-	public function has_bestuursfoto($bestuur)
-	{
-		return file_exists('public/images/boards/' . $bestuur->get('login') . '.jpg');
-	}
-
-	public function get_bestuursfoto($bestuur)
-	{
-		return 'images/boards/' . $bestuur->get('login') . '.jpg';
-	}
-
-	public function parse_bestuursfoto($bestuur, $html)
-	{
-		return preg_replace('/____BESTUURSFOTO____(\s+|\<br\/?\>)*/i', '', $html);
-	}
+	
 }
