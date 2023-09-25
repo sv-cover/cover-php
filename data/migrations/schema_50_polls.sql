97c8af7080a919b7fd881aa4f5b1183208fae5fe
Martijn Luinstra <martijnluinstra@gmail.com>
2023-09-25 23:15:31 +0200

    Migrate old polls from forum

--- data/migrations/schema_50_polls.sql
+++ data/migrations/schema_50_polls.sql
@@ -48,3 +48,90 @@ CREATE TABLE poll_comment_likes (
     -- no updated_on, likes cannot be updated
     CONSTRAINT poll_commment_like_uniq UNIQUE (poll_comment_id, member_id)
 );
+
+-- Transfer old polls from forum to new datastructure
+DO $$
+DECLARE
+    v_poll_forum_id integer;
+    v_thread record;
+    v_option record;
+    v_message record;
+    v_vote record;
+    v_poll_id integer;
+    v_poll_option_id integer;
+BEGIN
+    -- Get poll forum ID
+    SELECT CAST(configuratie.value AS integer) INTO v_poll_forum_id
+      FROM configuratie 
+     WHERE configuratie.key = 'poll_forum';
+
+    FOR v_thread IN
+        SELECT ft.*
+              ,fm.message
+          FROM forum_threads AS ft
+          LEFT JOIN (
+                SELECT DISTINCT ON (thread_id)
+                       thread_id
+                      ,message
+                  FROM forum_messages
+                 ORDER BY thread_id, id
+               ) AS fm ON ft.id = fm.thread_id
+         WHERE ft.forum_id = v_poll_forum_id
+           AND ft.poll = 1
+    LOOP
+        -- Create poll
+        INSERT INTO polls (member_id, committee_id, question, created_on, closed_on)
+            VALUES (
+                CASE -- member_id
+                    WHEN v_thread.author_type = 1 AND EXISTS (SELECT 1 FROM leden WHERE id = v_thread.author_id) THEN v_thread.author_id
+                    ELSE NULL
+                END,
+                CASE -- committee_id
+                    WHEN v_thread.author_type = 2 AND EXISTS (SELECT 1 FROM commissies WHERE id = v_thread.author_id) THEN v_thread.author_id
+                    ELSE NULL
+                END,
+                CASE -- question
+                    WHEN v_thread.subject NOT ILIKE v_thread.message THEN concat_ws(E'\n', v_thread.subject, v_thread.message)
+                    ELSE v_thread.subject
+                END,
+                v_thread.date, -- created_on
+                v_thread.date + INTERVAL '7 day' -- closed_on
+            )
+            RETURNING id INTO v_poll_id;
+        
+        -- Transfer options and votes
+        FOR v_option IN
+            SELECT *
+              FROM pollopties
+             WHERE pollid = v_thread.id
+        LOOP
+            INSERT INTO poll_options (poll_id, option)
+                VALUES (v_poll_id, v_option.optie)
+                RETURNING id INTO v_poll_option_id;
+
+            FOR i IN 0..(v_option.stemmen - 1) LOOP
+                INSERT INTO poll_votes (poll_option_id) VALUES (v_poll_option_id);
+            END LOOP;
+        END LOOP;
+
+        -- Transfer comments
+        FOR v_message IN
+            SELECT *
+              FROM forum_messages
+             WHERE thread_id = v_thread.id
+             ORDER BY id
+            OFFSET 1 -- First message is part of question
+        LOOP
+            INSERT INTO poll_comments (poll_id, member_id, comment, created_on)
+                VALUES (
+                    v_poll_id, --poll_id
+                    CASE -- member_id
+                        WHEN v_message.author_type = 1 AND EXISTS (SELECT 1 FROM leden WHERE id = v_thread.author_id) THEN v_message.author_id
+                        ELSE NULL
+                    END,
+                    v_message.message, -- comment
+                    v_message.date -- created_on
+                );
+        END LOOP;
+    END LOOP;
+END $$;
